2388f461125aeb610776d1fc24d983d236afc2e8
Allen Garvey <allengarvey@hotmail.com>
2017-12-20 13:20:16 +0000

    Add random threshold dither

--- js_src/threshold.js
+++ js_src/threshold.js
@@ -2,19 +2,26 @@ var App = App || {};
 
 App.Threshold = (function(Image, Pixel){
     
-    function thresholdImage(sourceContext, targetContext, imageWidth, imageHeight, threshold){
-        Image.transform(sourceContext, targetContext, imageWidth, imageHeight, (pixel, x, y)=>{
-            var lightness = Pixel.lightness(pixel);
-            
-            if(lightness > threshold){
-                return Pixel.create(255, 255, 255, pixel.a);
-            }
-            return Pixel.create(0, 0, 0, pixel.a);
-            
-        });
-    }    
+    function thresholdTransformGenerator(thresholdFunc){
+        return function(sourceContext, targetContext, imageWidth, imageHeight, threshold){
+            Image.transform(sourceContext, targetContext, imageWidth, imageHeight, (pixel, x, y)=>{
+                var lightness = Pixel.lightness(pixel);
+                
+                if(lightness > thresholdFunc(threshold)){
+                    return Pixel.create(255, 255, 255, pixel.a);
+                }
+                return Pixel.create(0, 0, 0, pixel.a);
+                
+            });
+        };
+    }
+    
+    function randomThresholdFunc(threshold){ 
+        return Math.ceil(Math.random() * threshold); 
+    }
     
     return {
-       image: thresholdImage
+       image: thresholdTransformGenerator(function(threshold){ return threshold; }),
+       randomDither: thresholdTransformGenerator(randomThresholdFunc),
     };
 })(App.Image, App.Pixel);
