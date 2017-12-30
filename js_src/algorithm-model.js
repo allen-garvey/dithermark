bd9ca4b7f6cde188937f706c4d641c122749fe63
Allen Garvey <allengarvey@hotmail.com>
2017-12-30 15:36:35 +0000

    Move list of dither algorithms into separate module

---
+++ js_src/algorithm-model.js
@@ -0,0 +1,71 @@
+App.AlgorithmModel = (function(WebGl){
+    var ditherAlgorithms = [
+                {
+                    title: "Threshold", 
+                    id: 1,
+                    webGlFunc: WebGl.threshold,
+                },
+                {
+                    title: "Random Threshold", 
+                    id: 2,
+                    webGlFunc: WebGl.randomThreshold,
+                },
+                {
+                    title: "Atkinson", 
+                    id: 3,
+                },
+                {
+                    title: "Floyd-Steinberg", 
+                    id: 4,
+                },
+                {
+            		title: "Javis-Judice-Ninke",
+            		id: 5,
+            	},
+            	{
+            		title: "Stucki",
+            		id: 6,
+            	},
+            	{
+            		title: "Burkes",
+            		id: 7,
+            	},
+            	{
+            		title: "Sierra3",
+            		id: 8,
+            	},
+            	{
+            		title: "Sierra2",
+            		id: 9,
+            	},
+            	{
+            		title: "Sierra1",
+            		id: 10,
+            	},
+            	{
+            	    title: "Ordered Dither 2x2",
+            	    id: 11,
+            	    webGlFunc: WebGl.orderedDither2,
+            	},
+            	{
+            	    title: "Ordered Dither 4x4",
+            	    id: 12,
+            	    webGlFunc: WebGl.orderedDither4,
+            	},
+            	{
+            	    title: "Ordered Dither 8x8",
+            	    id: 13,
+            	    webGlFunc: WebGl.orderedDither8,
+            	},
+            	{
+            	    title: "Ordered Dither 16x16",
+            	    id: 14,
+            	    webGlFunc: WebGl.orderedDither16,
+            	},
+            ];
+    
+    return {
+        ditherAlgorithms: ditherAlgorithms,
+    };
+    
+})(App.WebGl);
--- js_src/app.js
+++ js_src/app.js
@@ -1,4 +1,4 @@
-(function(Vue, Fs, Canvas, Timer, Histogram, Pixel, WorkerUtil, WebGl){
+(function(Vue, Fs, Canvas, Timer, Histogram, Pixel, WorkerUtil, WebGl, AlgorithmModel){
     
     //takes hex in form #ffffff and returns pixel
     function pixelFromColorPicker(hex){
@@ -69,70 +69,7 @@
             histogramWidth: Histogram.width,
             colorReplaceBlack: '',
             colorReplaceWhite: '',
-            ditherAlgorithms: [
-                {
-                    title: "Threshold", 
-                    id: 1,
-                    webGlFunc: WebGl.threshold,
-                },
-                {
-                    title: "Random Threshold", 
-                    id: 2,
-                    webGlFunc: WebGl.randomThreshold,
-                },
-                {
-                    title: "Atkinson", 
-                    id: 3,
-                },
-                {
-                    title: "Floyd-Steinberg", 
-                    id: 4,
-                },
-                {
-            		title: "Javis-Judice-Ninke",
-            		id: 5,
-            	},
-            	{
-            		title: "Stucki",
-            		id: 6,
-            	},
-            	{
-            		title: "Burkes",
-            		id: 7,
-            	},
-            	{
-            		title: "Sierra3",
-            		id: 8,
-            	},
-            	{
-            		title: "Sierra2",
-            		id: 9,
-            	},
-            	{
-            		title: "Sierra1",
-            		id: 10,
-            	},
-            	{
-            	    title: "Ordered Dither 2x2",
-            	    id: 11,
-            	    webGlFunc: WebGl.orderedDither2,
-            	},
-            	{
-            	    title: "Ordered Dither 4x4",
-            	    id: 12,
-            	    webGlFunc: WebGl.orderedDither4,
-            	},
-            	{
-            	    title: "Ordered Dither 8x8",
-            	    id: 13,
-            	    webGlFunc: WebGl.orderedDither8,
-            	},
-            	{
-            	    title: "Ordered Dither 16x16",
-            	    id: 14,
-            	    webGlFunc: WebGl.orderedDither16,
-            	},
-            ],
+            ditherAlgorithms: AlgorithmModel.ditherAlgorithms,
         },
         computed: {
             isImageLoaded: function(){
@@ -346,4 +283,4 @@
     fileInput.addEventListener('change', (e)=>{
         Fs.openImageFile(e, app.loadImage);   
     }, false);
-})(window.Vue, App.Fs, App.Canvas, App.Timer, App.Histogram, App.Pixel, App.WorkerUtil, App.WebGl);
+})(window.Vue, App.Fs, App.Canvas, App.Timer, App.Histogram, App.Pixel, App.WorkerUtil, App.WebGl, App.AlgorithmModel);
