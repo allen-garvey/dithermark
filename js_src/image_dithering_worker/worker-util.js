aa9845dcd786ec1c58991583b113747ccda10d22
Allen Garvey <allengarvey@hotmail.com>
2018-01-05 16:44:47 +0000

    improve speed of copying arraybuffer in webworker

--- js_src/image_dithering_worker/worker-util.js
+++ js_src/image_dithering_worker/worker-util.js
@@ -1,14 +1,16 @@
-App.WorkerUtil = (function(){
+App.WorkerUtil = (function(WorkerHeaders){
+    //based on: https://stackoverflow.com/questions/10100798/whats-the-most-straightforward-way-to-copy-an-arraybuffer-object
     function copyBuffer(pixelBufferOriginal){
-        var ditheredPixelBuffer = new App.Polyfills.SharedArrayBuffer(pixelBufferOriginal.byteLength);
-        var ditheredPixels = new Uint8ClampedArray(ditheredPixelBuffer);
-        var originalPixels = new Uint8ClampedArray(pixelBufferOriginal);
-        for(let i=0;i<originalPixels.length;i++){
-            ditheredPixels[i] = originalPixels[i];
-        }
-        return  {buffer: ditheredPixelBuffer, pixels: ditheredPixels};
+        //faster than using for loop
+        var copiedBuffer = new ArrayBuffer(pixelBufferOriginal.byteLength);
+        var copiedPixels = new Uint8Array(copiedBuffer);
+        copiedPixels.set(new Uint8Array(pixelBufferOriginal));
+        return {buffer: copiedBuffer, pixels: copiedPixels};
+        
+        
+        
     }
     return {
         copyBuffer: copyBuffer,
     };
-})();
+})(App.WorkerHeaders);
