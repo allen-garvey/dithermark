34478c68d6771a61aebac9400fd61416b36e91e2
Allen Garvey <allengarvey@hotmail.com>
2018-01-26 21:10:33 +0000

    Delete histogram worker file as it is no longer used

--- js_src/histogram_worker/histogram-main.js
+++
@@ -1,9 +0,0 @@
-
-onmessage = function(e) {
-    var messageData = e.data;
-    var imageBuffer = messageData;
-    var pixels = new Uint8ClampedArray(imageBuffer);
-    var histogramBuffer = App.Histogram.createHistogram(pixels);
-    
-    postMessage(histogramBuffer);
-}
