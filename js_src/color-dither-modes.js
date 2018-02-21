e319faca0fedd80f47c108f0f002a5e929d0e36c
Allen Garvey <allengarvey@hotmail.com>
2018-02-21 02:10:20 +0000

    Reorder color dither modes

--- js_src/color-dither-modes.js
+++ js_src/color-dither-modes.js
@@ -1,8 +1,8 @@
 App.ColorDitherModes = (function(){
     let ret = new Map();
-    ret.set('RGB', {id: 0, title: 'RGB'});
-    ret.set('HSL', {id: 1, title: 'Weighted HSL'});
-    ret.set('HUE_LIGHTNESS', {id: 2, title: 'Hue & Lightness'});
     ret.set('LIGHTNESS', {id:3, title: 'Lightness'});
+    ret.set('HUE_LIGHTNESS', {id: 2, title: 'Hue & Lightness'});
+    ret.set('HSL', {id: 1, title: 'Weighted HSL'});
+    ret.set('RGB', {id: 0, title: 'RGB'});
     return ret;
 })();
