264c00bbe4cf634d84638c7dc767dbddecf6348b
Allen Garvey <allengarvey@hotmail.com>
2018-03-02 22:29:56 +0000

    Add worker header for color dither

--- js_src/shared/worker-headers.js
+++ js_src/shared/worker-headers.js
@@ -1,7 +1,8 @@
 App.WorkerHeaders = {
     LOAD_IMAGE: 0,
-    DITHER: 1,
-    DITHER_BW: 2,
+    DITHER: 1, //somewhat confusingly, this is for dithering in black and white
+    DITHER_BW: 2, //for dithering in black and white, where black and white replacement values are not set
     HISTOGRAM: 3,
     HUE_HISTOGRAM: 4,
+    DITHER_COLOR: 5,
 };
