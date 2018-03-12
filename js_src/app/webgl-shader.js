fa3bbd72e87df5e741a2d2f00e8616874f18950b
Allen Garvey <allengarvey@hotmail.com>
2018-03-12 16:27:32 +0000

    Refactor webgl bw arithmetic dither to put fragment shader return statements in shared shader module

--- js_src/app/webgl-shader.js
+++ js_src/app/webgl-shader.js
@@ -20,5 +20,11 @@ App.WebGlShader = (function(){
         shaderText: shaderText,
         vertexShaderText: shaderText('webgl-threshold-vertex-shader'),
         generateBitwiseFunctionsText: generateBitwiseFunctionsText,
+        aDitherAdd1Return: 'aDitherMask3(x, y)',
+        aDitherAdd2Return: '(aDitherMask4(x, y, 0) + aDitherMask4(x, y, 1) + aDitherMask4(x, y, 2)) / 3.0',
+        aDitherAdd3Return: '(aDitherMask4(x, y, int(pixel.r * 255.)) + aDitherMask4(x, y, int(pixel.g * 255.)) + aDitherMask4(x, y, int(pixel.b * 255.))) / 3.0',
+        aDitherXor1Return: 'aDitherMask1(x, y)',
+        aDitherXor2Return: '(aDitherMask2(x, y, 0) + aDitherMask2(x, y, 1) + aDitherMask2(x, y, 2)) / 3.0',
+        aDitherXor3Return: '(aDitherMask2(x, y, int(pixel.r * 255.)) + aDitherMask2(x, y, int(pixel.g * 255.)) + aDitherMask2(x, y, int(pixel.b * 255.))) / 3.0',
     };
 })();
