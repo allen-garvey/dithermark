24abf1598b8f8c82966574b9cb35512c6fbfc32b
Allen Garvey <allengarvey@hotmail.com>
2018-03-15 21:09:18 +0000

    Version 2 of optimize palette functionality

--- js_src/image_dithering_worker/optimize-palette.js
+++ js_src/image_dithering_worker/optimize-palette.js
@@ -57,6 +57,50 @@ App.OptimizePalette = (function(Pixel, PixelMath){
         return buckets;
     }
     
+    function uniformPopularityBase(pixels, numColors, numDistinctValues, pixelValueFunc){
+        let popularityMap = new Float32Array(numDistinctValues);
+        
+        for(let i=0;i<pixels.length;i+=4){
+            let pixel = pixels.subarray(i, i+5);
+            let pixelValue = pixelValueFunc(pixel);
+            // if(pixelValue < 0 || pixelValue >= popularityMap.length || typeof pixelValue !== 'number' || isNaN(pixelValue)){
+            //     console.log(`pixel value out of range ${pixelValue}/${numDistinctValues}`);
+            // }
+            popularityMap[pixelValue] = popularityMap[pixelValue] + 1;
+        }
+        // console.log(popularityMap);
+        // console.log(popularityMap.reduce((sum, current)=>{ return sum+ current;}, 0));
+        let buckets;
+        if(numDistinctValues <= 255){
+            buckets = new Uint8Array(numColors);   
+        }
+        else{
+            buckets = new Uint16Array(numColors);
+        }
+        let valueMin;
+        let valueMax;
+        for(let i=0;i<popularityMap.length;i++){
+            if(popularityMap[i] > 0){
+                valueMin = i;
+                break;   
+            }
+        }
+        
+        for(let i=popularityMap.length-1;i>=0;i--){
+            if(popularityMap[i] > 0){
+                valueMax = i;
+                break;   
+            }
+        }
+        let bucketFraction = Math.floor((valueMax - valueMin) / buckets.length);
+        buckets = buckets.map((value, i)=>{ return i * bucketFraction; });
+        //rounding down will make this less than the max value
+        buckets[buckets.length -1] = valueMax;
+        return buckets;
+    }
+    
+    
+    
     //zips lightnesses and saturation arrays into single array
     //both arrays should be the same length
     //based on artistic/psychological principle that very dark or very light colors should be less saturated
@@ -78,8 +122,9 @@ App.OptimizePalette = (function(Pixel, PixelMath){
             ret[ret.length - 1 - retIndex] = saturations[saturationIndex + 1];
         }
         //for odd numbers
-        if(saturationIndex < saturations.length - 1){
-            ret[retIndex + 3] = saturations[saturationIndex + 1];
+        if(ret.length % 2 === 1){
+            let middle = half + 1;
+            ret[middle * 3 + 1] = saturations[middle];
         }
         
         
@@ -91,6 +136,32 @@ App.OptimizePalette = (function(Pixel, PixelMath){
         return ret;
     }
     
+    //based on: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    function shuffle(array) {
+        let currentIndex = array.length;
+        let temporaryValue;
+        let randomIndex;
+        
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+        
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+        
+        return array;
+    }
+    
+    function averageArrays(array1, array2){
+        return array1.map((value, index)=>{ return Math.floor((value + array2[index]) / 2); });
+    }
+    
     function medianPopularity(pixels, numColors){
         let logarithmicBucketCapacityFunc = (numPixels, numBuckets, currentBucketNum, previousBucketCapacity)=>{
                 previousBucketCapacity = previousBucketCapacity > 0 ? previousBucketCapacity : numPixels;
@@ -99,9 +170,13 @@ App.OptimizePalette = (function(Pixel, PixelMath){
         
         
         let lightnesses = medianPopularityBase(pixels, numColors, 256, PixelMath.lightness);
+        // let lightnesses2 = uniformPopularityBase(pixels, numColors, 256, PixelMath.lightness);
+        // lightnesses = averageArrays(lightnesses, lightnesses2);
         console.log('lightness results');
         console.log(lightnesses);
-        let saturations = medianPopularityBase(pixels, numColors, 101, PixelMath.saturation, logarithmicBucketCapacityFunc);
+        let saturations = uniformPopularityBase(pixels, numColors, 101, PixelMath.saturation, logarithmicBucketCapacityFunc);
+        let saturations2 = medianPopularityBase(pixels, numColors, 101, PixelMath.saturation, logarithmicBucketCapacityFunc);
+        saturations = averageArrays(saturations, saturations2);
         console.log('saturation results');
         console.log(saturations);
         let hues = medianPopularityBase(pixels, numColors, 360, (pixel)=>{
@@ -111,9 +186,23 @@ App.OptimizePalette = (function(Pixel, PixelMath){
             }
             return value;
         });
+        let hues2 = uniformPopularityBase(pixels, numColors, 360, (pixel)=>{
+            let value = PixelMath.hue(pixel);
+            if(value === 360){
+                return 0;
+            }
+            return value;
+        });
+        hues = averageArrays(hues, hues2);
+        shuffle(hues);
         console.log('hue results');
         console.log(hues);
-        let ret = PixelMath.hslArrayToRgb(zipHsl(hues, saturations, lightnesses, numColors));
+        let hsl = zipHsl(hues, saturations, lightnesses, numColors);
+        console.log('hsl');
+        console.log(hsl);
+        let ret = PixelMath.hslArrayToRgb(hsl);
+        // console.log('rgb');
+        // console.log(ret);
         return ret;
     }
     
