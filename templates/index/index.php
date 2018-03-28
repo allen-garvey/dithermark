ca4c1dec8fa07099f932dec73612ff12bf819e2f
Allen Garvey <allen.garvey@gmail.com>
2018-03-28 13:49:33 -0400

    Change styles and font for app name in header

--- templates/index/index.php
+++ templates/index/index.php
@@ -9,7 +9,6 @@
         <title>jsdither</title>
         <meta name="description" content=""/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="https://fonts.googleapis.com/css?family=Bungee&text=jsdither" rel="stylesheet"/>
         <link rel="stylesheet" type="text/css" href="/styles/style.css"/>
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     </head>
