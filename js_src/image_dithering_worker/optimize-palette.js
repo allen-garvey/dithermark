ada34196b4bda8d0af7d838f1953cbd883c5c8c9
Allen Garvey <allen.garvey@gmail.com>
2018-03-29 12:46:29 -0400

    Remove array shuffling function in optimize palette since it is unused

--- js_src/image_dithering_worker/optimize-palette.js
+++ js_src/image_dithering_worker/optimize-palette.js
@@ -210,27 +210,6 @@ App.OptimizePalette = (function(Pixel, PixelMath){
         return ret;
     }
     
-    //based on: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    function shuffle(array) {
-        let currentIndex = array.length;
-        let temporaryValue;
-        let randomIndex;
-        
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-        
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-        }
-        
-        return array;
-    }
     
     //weight is how much to weight towards first array
     function averageArrays(array1, array2, weight=1){
