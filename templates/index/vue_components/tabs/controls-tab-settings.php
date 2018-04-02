c654ac0db2992f97e3a7197319a3d91a74c1fa0f
Allen Garvey <allen.garvey@gmail.com>
2018-04-02 11:19:31 -0400

    Add fieldset for global settings tab

--- templates/index/vue_components/tabs/controls-tab-settings.php
+++ templates/index/vue_components/tabs/controls-tab-settings.php
@@ -1,17 +1,21 @@
-<div>
-    <label>Editor theme
+<fieldset>
+    <legend>Appearance</legend>
+    <label>Theme
         <select v-model.number="currentEditorThemeIndex">
             <template v-for="(theme, index) in editorThemes">
                 <option v-bind:value="index">{{theme.name}}</option>
             </template>
         </select>
     </label>
-</div>
-<div class="spread-content">
-    <label>Live preview
-        <input type="checkbox" v-model="isLivePreviewEnabled"/>
-    </label>
-    <label v-if="isWebglSupported">Enable WebGL
-        <input type="checkbox" v-model="isWebglEnabled"/>
-    </label>
-</div>
+</fieldset>
+<fieldset>
+    <legend>Performance</legend>
+    <div class="spread-content">
+        <label>Live preview
+            <input type="checkbox" v-model="isLivePreviewEnabled"/>
+        </label>
+        <label v-if="isWebglSupported">Enable WebGL
+            <input type="checkbox" v-model="isWebglEnabled"/>
+        </label>
+    </div>
+</fieldset>
