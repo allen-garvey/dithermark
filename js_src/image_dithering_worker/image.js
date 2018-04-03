a9f09f8281b5b2f5afc5dadf09a4c620277381a8
Allen Garvey <allen.garvey@gmail.com>
2018-04-03 15:36:55 -0400

    Update worker image module to ES2015 syntax

--- js_src/image_dithering_worker/image.js
+++ js_src/image_dithering_worker/image.js
@@ -1,17 +1,17 @@
 
 App.Image = (function(Pixel){
     function transformImage(pixels, imageWidth, imageHeight, pixelTransformFunc){
-        var y = 0;
-        var x = 0;
-        var pixel = Pixel.create(0, 0, 0);
+        let y = 0;
+        let x = 0;
+        let pixel = Pixel.create(0, 0, 0);
         
-        for(var i=0;i<pixels.length;i+=4){
+        for(let i=0;i<pixels.length;i+=4){
             pixel[Pixel.R_INDEX] = pixels[i];
             pixel[Pixel.G_INDEX] = pixels[i+1];
             pixel[Pixel.B_INDEX] = pixels[i+2];
             pixel[Pixel.A_INDEX] = pixels[i+3];
             
-            var outputPixel = pixelTransformFunc(pixel, x, y);
+            let outputPixel = pixelTransformFunc(pixel, x, y);
             
             pixels[i] = outputPixel[0];
             pixels[i+1] = outputPixel[1];
