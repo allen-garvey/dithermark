438df209670cbff8e2ea0b9cf919e2cf2a8831c5
Allen Garvey <allen.garvey@gmail.com>
2018-04-04 11:37:05 -0400

    Add initial implementation of webworker color ordered dithers

--- js_src/image_dithering_worker/ordered-dither.js
+++ js_src/image_dithering_worker/ordered-dither.js
@@ -57,12 +57,54 @@ App.OrderedDither = (function(Image, Pixel, Bayer, PixelMath){
             return createOrderedDitherBase(dimensions, Bayer[bayerFuncName]);
         };
     }
+
+
+    /**
+     * Color dither stuff
+     */
+    function convertBayerToFloat(bayerMatrix){
+        const length = bayerMatrix.length;
+        const highestValue = length;
+        let floatData = new Float32Array(length);
+        
+        bayerMatrix.forEach((value, i)=>{
+            floatData[i] = (value + 1) / highestValue;
+        });
+        return floatData;
+    }
+
+    function createColorOrderedDitherBase(dimensions, bayerCreationFunc){
+        let matrix = createMaxtrix(dimensions, convertBayerToFloat(bayerCreationFunc(dimensions)));
+
+        return (pixels, imageWidth, imageHeight, colorDitherModeId, colors)=>{
+            return Image.colorDither(pixels, imageWidth, imageHeight, colorDitherModeId, colors, (closestColor, secondClosestColor, closestDistance, secondClosestDistance, x, y)=>{
+                const matrixFraction = matrixValue(matrix, x % matrix.dimensions, y % matrix.dimensions);
+                if(matrixFraction * secondClosestDistance < closestDistance){
+                    return secondClosestColor;
+                }
+                return closestColor;
+            });
+        };
+    }
+
+    function colorOrderedDitherBuilder(bayerFuncName){
+        return function(dimensions){
+            return createColorOrderedDitherBase(dimensions, Bayer[bayerFuncName]);
+        };
+    }
+
     
     return {
+        //bw dither
         createOrderedDither: orderedDitherBuilder('create'),
         createClusterOrderedDither: orderedDitherBuilder('createCluster'),
         createDotClusterOrderedDither: orderedDitherBuilder('createDotCluster'),
         createPatternOrderedDither: orderedDitherBuilder('createPattern'),
+        //color dither
+        createColorOrderedDither: colorOrderedDitherBuilder('create'),
+        createColorClusterOrderedDither: colorOrderedDitherBuilder('createCluster'),
+        createColorDotClusterOrderedDither: colorOrderedDitherBuilder('createDotCluster'),
+        createColorPatternOrderedDither: colorOrderedDitherBuilder('createPattern'),
     };
     
 })(App.Image, App.Pixel, App.BayerMatrix, App.PixelMath);
