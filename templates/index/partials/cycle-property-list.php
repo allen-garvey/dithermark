b64878ee22465e4eb285aed6c4592309817e067f
Allen Garvey <allen.garvey@gmail.com>
2018-04-13 13:14:58 -0400

    Rename css class for cycle property list button to better reflect function

--- templates/index/partials/cycle-property-list.php
+++ templates/index/partials/cycle-property-list.php
@@ -1,4 +1,4 @@
 <div>
-    <button class="shuffle-color-palette-button" title="Previous <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsPreviousArgs; ?>)"><</button>
-    <button class="shuffle-color-palette-button" title="Next <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsNextArgs; ?>)">></button>
+    <button class="cycle-property-list-button" title="Previous <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsPreviousArgs; ?>)"><</button>
+    <button class="cycle-property-list-button" title="Next <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsNextArgs; ?>)">></button>
 </div>
