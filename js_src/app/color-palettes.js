4b76e6e2fdc302f3d2dcd35c49c298819f540590
Allen Garvey <allen.garvey@gmail.com>
2018-04-18 12:56:54 -0400

    Reorder colors in uniform color palette

--- js_src/app/color-palettes.js
+++ js_src/app/color-palettes.js
@@ -3,7 +3,7 @@ App.ColorPalettes = (function(){
     function getPalettes(){
         return [
             {title: 'Custom', isCustom: true},
-            {title: 'Uniform', colors: ['#201e22', '#ebe1e6', '#2d5b2d', '#795633', '#2c3838', '#a82828', '#267ace', '#b3a2c9', '#62eaa6', '#ebeb2d', '#d69e75', '#a7c38b']},
+            {title: 'Uniform', colors: ['#201e22', '#ebe1e6', '#a82828', '#ebeb2d', '#795633', '#2d5b2d', '#267ace', '#d69e75', '#2c3838', '#62c7ea', '#b3a2c9', '#a7c38b']},
             {title: 'Uniform 2', colors: ['#211e22', '#e3ebe1', '#32423e', '#d1dfa5', '#2e3a5e', '#793368', '#2888a8', '#ce5026', '#ebbc2d', '#54e879', '#9a85db', '#e0c8ce']},
             {title: 'Primaries', colors: ['#000000', '#ffffff', '#c40000', '#000075', '#ffff8a', '#6f207a', '#f8c2f7', '#22531a', '#ff8800', '#40321e', '#485ca4', '#87d255']},
             {title: 'Galaxy', colors: ['#0d3677', '#D2ebf0', '#763a70', '#f9d2f4', '#242440', '#cd20cd', '#c5c565', '#b17633', '#554d28', '#2e337a', '#fefde9', '#181823']},
