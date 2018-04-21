0cf524cbc6c84679293ce6b321216a02a333e4ec
Allen Garvey <allen.garvey@gmail.com>
2018-04-21 11:22:22 -0400

    Refactor dither studio template to use vue attribute shorthand syntax

--- templates/index/vue_components/dither-studio-component.php
+++ templates/index/vue_components/dither-studio-component.php
@@ -4,7 +4,7 @@
             <div class="global-controls-panel controls-panel">
                 <div class="tabs-container">
                     <template v-for="(tab, index) in globalControlsTabs">
-                        <div class="tab" v-bind:class="{active: activeControlsTab === index, disabled: tab.isDisabled}" v-on:click="setActiveControlsTab(index, tab.isDisabled)">{{tab.name}}</div>
+                        <div class="tab" :class="{active: activeControlsTab === index, disabled: tab.isDisabled}" @click="setActiveControlsTab(index, tab.isDisabled)">{{tab.name}}</div>
                     </template>
                 </div>
                 <?php foreach(['open', 'image', 'settings', 'export'] as $index => $templateName): ?>
@@ -15,28 +15,28 @@
             </div>
             <div class="super-dither-controls-container" v-show="isImageLoaded">
                 <div class="tabs-container">
-                    <div class="tab" v-bind:class="{active: activeDitherTab === 0}" v-on:click="loadDitherTab(0)">BW Dither</div>
-                    <div class="tab" v-bind:class="{active: activeDitherTab === 1}" v-on:click="loadDitherTab(1)">Color Dither</div>
+                    <div class="tab" :class="{active: activeDitherTab === 0}" @click="loadDitherTab(0)">BW Dither</div>
+                    <div class="tab" :class="{active: activeDitherTab === 1}" @click="loadDitherTab(1)">Color Dither</div>
                 </div>
                 <div v-show="activeDitherTab === 0">
-                    <bw-dither-section ref="bwDitherSection" v-on:request-worker="onWorkerRequested" v-on:display-transformed-image="zoomImage" v-bind:transform-canvas-web-gl="transformCanvasWebGl" v-bind:transform-canvas="transformCanvas" v-bind:source-canvas="sourceCanvas" v-bind:is-webgl-enabled="isWebglEnabled" v-bind:is-webgl-supported="isWebglSupported" v-bind:is-live-preview-enabled="isLivePreviewEnabled" />  
+                    <bw-dither-section ref="bwDitherSection" @request-worker="onWorkerRequested" @display-transformed-image="zoomImage" :transform-canvas-web-gl="transformCanvasWebGl" :transform-canvas="transformCanvas" :source-canvas="sourceCanvas" :is-webgl-enabled="isWebglEnabled" :is-webgl-supported="isWebglSupported" :is-live-preview-enabled="isLivePreviewEnabled" />  
                 </div>
                 <div v-show="activeDitherTab === 1">
-                    <color-dither-section ref="colorDitherSection" v-on:request-worker="onWorkerRequested" v-on:display-transformed-image="zoomImage" v-bind:transform-canvas-web-gl="transformCanvasWebGl" v-bind:transform-canvas="transformCanvas" v-bind:source-canvas="sourceCanvas" v-bind:is-webgl-enabled="isWebglEnabled" v-bind:is-webgl-supported="isWebglSupported" v-bind:is-live-preview-enabled="isLivePreviewEnabled" />
+                    <color-dither-section ref="colorDitherSection" @request-worker="onWorkerRequested" @display-transformed-image="zoomImage" :transform-canvas-web-gl="transformCanvasWebGl" :transform-canvas="transformCanvas" :source-canvas="sourceCanvas" :is-webgl-enabled="isWebglEnabled" :is-webgl-supported="isWebglSupported" :is-live-preview-enabled="isLivePreviewEnabled" />
                 </div>
             </div>
         </div>
         <div class="zoom-bar-container" v-show="isImageLoaded">
             <label>
                 Zoom
-                <input type="range" v-bind:min="zoomMin" v-bind:max="zoomMax" v-model.number="zoom"/>
-                <input type="number" v-bind:min="zoomMin" v-bind:max="zoomMax" v-model.number="zoomDisplay" @keyup.enter="zoom = zoomDisplay"/>
-                <button v-show="zoom !== 100" v-on:click="resetZoom">Reset</button>
+                <input type="range" :min="zoomMin" :max="zoomMax" v-model.number="zoom"/>
+                <input type="number" :min="zoomMin" :max="zoomMax" v-model.number="zoomDisplay" @keyup.enter="zoom = zoomDisplay"/>
+                <button v-show="zoom !== 100" @click="resetZoom">Reset</button>
             </label>
         </div>
     </div>
         <div class="image-canvas-supercontainer" v-show="isImageLoaded">
-            <div class="image-canvas-container" v-bind:class="{'show-original': showOriginalImage}">
+            <div class="image-canvas-container" :class="{'show-original': showOriginalImage}">
                 <canvas ref="saveImageCanvas" class="hidden"></canvas><?php //used when saving image, so pixelated images are scaled correctly ?>
                 <canvas ref="originalImageCanvas" class="hidden"></canvas><?php //original non-pixelated image loaded by user ?>
                 <canvas ref="sourceCanvas" class="hidden"></canvas><?php //pixelated-image used as source to dithers ?>
