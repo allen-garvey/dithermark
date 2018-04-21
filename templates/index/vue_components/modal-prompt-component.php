9507cc374107a7c6e6de6079047e0e90b0920c66
Allen Garvey <allen.garvey@gmail.com>
2018-04-21 12:02:45 -0400

    Add tab indexes to modal prompt

--- templates/index/vue_components/modal-prompt-component.php
+++ templates/index/vue_components/modal-prompt-component.php
@@ -1,12 +1,12 @@
-<div v-show="showModal" @keyup.esc="cancelAction" class="modal-layout-container">
+<div v-if="showModal" @keyup.esc="cancelAction" class="modal-layout-container">
     <div class="modal-overlay" @click="cancelAction"></div>
     <div class="modal-container">
         <label>{{labelValue}}
-            <input :type="inputType" :placeholder="placeholder" v-model="inputValue" @keyup.enter="okAction" />
+            <input ref="input" tabindex="1" :type="inputType" :placeholder="placeholder" v-model="inputValue" @keyup.enter="okAction" />
         </label>
         <div class="modal-buttons-container">
-            <button @click="cancelAction">Cancel</button>
-            <button @click="okAction">{{okButtonValue}}</button>
+            <button tabindex="2" @click="cancelAction">Cancel</button>
+            <button tabindex="3" @click="okAction">{{okButtonValue}}</button>
         </div>
     </div>
 </div>
