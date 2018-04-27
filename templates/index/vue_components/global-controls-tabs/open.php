8cd547baa3035f57b7d17e80c2e2f43af0b8d4e1
Allen Garvey <allen.garvey@gmail.com>
2018-04-27 13:23:24 -0400

    Move open buttons in global controls open tab into fieldsets

--- templates/index/vue_components/global-controls-tabs/open.php
+++ templates/index/vue_components/global-controls-tabs/open.php
@@ -1,5 +1,11 @@
 <div>
-    <button class="btn btn-primary" v-on:click="loadImageTrigger" title="Open local image file">Open image</button>
-    <button class="btn btn-default" v-on:click="showOpenImageUrlPrompt" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load image from Url">Open image url</button>
-    <button class="btn btn-default" v-on:click="loadRandomImage" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load random image from Unsplash">Random image</button>    
+    <fieldset>
+        <legend>Local</legend>
+        <button class="btn btn-primary" v-on:click="loadImageTrigger" title="Open local image file">Open image</button>
+    </fieldset>
+    <fieldset>
+        <legend>Web</legend>
+        <button class="btn btn-default" v-on:click="showOpenImageUrlPrompt" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load image from Url">Open image url</button>
+    <button class="btn btn-default" v-on:click="loadRandomImage" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load random image from Unsplash">Random image</button>
+    </fieldset>    
 </div>
