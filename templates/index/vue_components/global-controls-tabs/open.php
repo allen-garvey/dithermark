17b9e556994230cd554169b4549b02bb31278083
Allen Garvey <allen.garvey@gmail.com>
2018-04-30 10:34:52 -0400

    Edit verbiage for open image buttons

--- templates/index/vue_components/global-controls-tabs/open.php
+++ templates/index/vue_components/global-controls-tabs/open.php
@@ -1,11 +1,11 @@
 <div>
     <fieldset>
-        <legend>Local</legend>
-        <button class="btn btn-primary" v-on:click="loadImageTrigger" title="Open local image file">Open image</button>
+        <legend>Device</legend>
+        <button class="btn btn-primary" v-on:click="loadImageTrigger" title="Open local image file">Image file</button>
     </fieldset>
     <fieldset>
         <legend>Web</legend>
-        <button class="btn btn-default" v-on:click="showOpenImageUrlPrompt" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load image from Url">Open image url</button>
+        <button class="btn btn-default" v-on:click="showOpenImageUrlPrompt" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load image from Url">Image url</button>
     <button class="btn btn-default" v-on:click="loadRandomImage" v-bind:disabled="isCurrentlyLoadingImageUrl" title="Load random image from Unsplash">Random image</button>
     </fieldset>    
 </div>
