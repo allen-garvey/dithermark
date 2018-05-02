4be86f0529a0ccfd4783359b9cb504d57abd90c5
Allen Garvey <allen.garvey@gmail.com>
2018-05-02 13:13:41 -0400

    Add documentation for worker headers

--- js_src/shared/worker-headers.js
+++ js_src/shared/worker-headers.js
@@ -2,8 +2,8 @@ App.WorkerHeaders = {
     LOAD_IMAGE: 0,
     DITHER: 1, //somewhat confusingly, this is for dithering in black and white
     DITHER_BW: 2, //for dithering in black and white, where black and white replacement values are not set
-    HISTOGRAM: 3,
-    HUE_HISTOGRAM: 4,
-    DITHER_COLOR: 5,
-    OPTIMIZE_PALETTE: 6,
+    HISTOGRAM: 3, //bw dither lightness histogram
+    HUE_HISTOGRAM: 4, //color dither hue histogram
+    DITHER_COLOR: 5, //dithering in color
+    OPTIMIZE_PALETTE: 6, //color dither optimize palette
 };
