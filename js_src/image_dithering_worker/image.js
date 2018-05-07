b14962770cadde27080256b3e6065c6672aafea1
Allen Garvey <allen.garvey@gmail.com>
2018-05-06 20:08:18 -0400

    Fix worker dithers so fully transparent pixels are skipped

--- js_src/image_dithering_worker/image.js
+++ js_src/image_dithering_worker/image.js
@@ -6,17 +6,20 @@ App.Image = (function(Pixel, ColorDitherModeFunctions, DitherUtil, PixelMath){
         let pixel = Pixel.create(0, 0, 0);
         
         for(let i=0;i<pixels.length;i+=4){
-            pixel[Pixel.R_INDEX] = pixels[i];
-            pixel[Pixel.G_INDEX] = pixels[i+1];
-            pixel[Pixel.B_INDEX] = pixels[i+2];
-            pixel[Pixel.A_INDEX] = pixels[i+3];
+            //nothing needs to be done for transparent pixels
+            if(pixels[i+3] > 0){
+                pixel[Pixel.R_INDEX] = pixels[i];
+                pixel[Pixel.G_INDEX] = pixels[i+1];
+                pixel[Pixel.B_INDEX] = pixels[i+2];
+                pixel[Pixel.A_INDEX] = pixels[i+3];
+                
+                let outputPixel = pixelTransformFunc(pixel, x, y);
             
-            let outputPixel = pixelTransformFunc(pixel, x, y);
-            
-            pixels[i] = outputPixel[0];
-            pixels[i+1] = outputPixel[1];
-            pixels[i+2] = outputPixel[2];
-            pixels[i+3] = outputPixel[3];
+                pixels[i] = outputPixel[0];
+                pixels[i+1] = outputPixel[1];
+                pixels[i+2] = outputPixel[2];
+                pixels[i+3] = outputPixel[3];
+            }
             
             x++;
             if(x >= imageWidth){
@@ -62,26 +65,28 @@ App.Image = (function(Pixel, ColorDitherModeFunctions, DitherUtil, PixelMath){
         let pixel = Pixel.create(0, 0, 0);
         
         for(let i=0;i<pixels.length;i+=4){
-            pixel[Pixel.R_INDEX] = pixels[i];
-            pixel[Pixel.G_INDEX] = pixels[i+1];
-            pixel[Pixel.B_INDEX] = pixels[i+2];
-            // pixel[Pixel.A_INDEX] = pixels[i+3]; //not necessary to set alpha
+            //nothing needs to be done for transparent pixels
+            if(pixels[i+3] > 0){
+                pixel[Pixel.R_INDEX] = pixels[i];
+                pixel[Pixel.G_INDEX] = pixels[i+1];
+                pixel[Pixel.B_INDEX] = pixels[i+2];
+                // pixel[Pixel.A_INDEX] = pixels[i+3]; //not necessary to set alpha
 
-            const pixelAdjustmentValue = pixelAdjustmentFunc(x, y, pixel) * ditherRCoefficient;
-            if(pixelAdjustmentValue !== 0){
-                pixel[Pixel.R_INDEX] = PixelMath.clamp(pixel[Pixel.R_INDEX] + pixelAdjustmentValue);
-                pixel[Pixel.G_INDEX] = PixelMath.clamp(pixel[Pixel.G_INDEX] + pixelAdjustmentValue);
-                pixel[Pixel.B_INDEX] = PixelMath.clamp(pixel[Pixel.B_INDEX] + pixelAdjustmentValue);
+                const pixelAdjustmentValue = pixelAdjustmentFunc(x, y, pixel) * ditherRCoefficient;
+                if(pixelAdjustmentValue !== 0){
+                    pixel[Pixel.R_INDEX] = PixelMath.clamp(pixel[Pixel.R_INDEX] + pixelAdjustmentValue);
+                    pixel[Pixel.G_INDEX] = PixelMath.clamp(pixel[Pixel.G_INDEX] + pixelAdjustmentValue);
+                    pixel[Pixel.B_INDEX] = PixelMath.clamp(pixel[Pixel.B_INDEX] + pixelAdjustmentValue);
+                }
+                const closestColor = colors[findClosestColorIndex(pixelValueFunc(pixel), colorValues, pixelDistanceFunc)];
+                //postscriptFunc is for hue-lightness dither
+                const outputPixel = postscriptFunc(closestColor, x, y, pixel);
+
+                pixels[i] = outputPixel[0];
+                pixels[i+1] = outputPixel[1];
+                pixels[i+2] = outputPixel[2];
+                pixels[i+3] = pixels[i+3];
             }
-            
-            const closestColor = colors[findClosestColorIndex(pixelValueFunc(pixel), colorValues, pixelDistanceFunc)];
-            //postscriptFunc is for hue-lightness dither
-            const outputPixel = postscriptFunc(closestColor, x, y, pixel);
-
-            pixels[i] = outputPixel[0];
-            pixels[i+1] = outputPixel[1];
-            pixels[i+2] = outputPixel[2];
-            pixels[i+3] = pixels[i+3];
             
             x++;
             if(x >= imageWidth){
