d8c6a27610806794adef30b66f3f2c4dccb11356
Allen Garvey <allen.garvey@gmail.com>
2018-05-07 22:04:50 -0400

    Add script to get random image urls using Unsplash Api

---
+++ scripts/unsplash-random-images.js
@@ -0,0 +1,42 @@
+const request = require('request');
+const unsplashKeys = require('../inc/unsplash-api-secret.json');
+
+const unsplashApiUrlBase = 'https://api.unsplash.com';
+const randomPhotoUrl = `${unsplashApiUrlBase}/photos/random?featured=true&count=30`;
+
+const options = {
+    url: randomPhotoUrl,
+    headers: {
+      'Accept-Version': 'v1',
+      'Authorization': `Client-ID ${unsplashKeys.accessKey}`
+    }
+};
+
+
+function formatRandomImageItem(imageItem){
+    return {
+        id: imageItem.id,
+        urls: {
+            regular: imageItem.urls.regular,
+            small: imageItem.urls.small,
+        },
+        link: imageItem.links.html,
+        author: {
+            name: imageItem.user.name,
+            link: imageItem.user.links.html,
+        },
+    };
+}
+
+
+function randomImageCallback(error, response, body) {
+    if(!error && response.statusCode === 200){
+        const json = JSON.parse(body);
+        console.log(JSON.stringify(json.map(formatRandomImageItem)));
+    }
+    else{
+        console.log(error);
+    }
+}
+
+request(options, randomImageCallback);
