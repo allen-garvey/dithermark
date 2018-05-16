0b50d05ca50cf822bd83458b3ed7cb4fa47f8ef9
Allen Garvey <allen.garvey@gmail.com>
2018-05-16 13:06:19 -0400

    Add favicon and apple touch icon

--- templates/index/index.php
+++ templates/index/index.php
@@ -11,7 +11,7 @@
         <meta name="description" content="An interactive, in-browser exploration of image dithering and color quantization algorithms"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" type="text/css" href="<?= CSS_APP_URL; ?>"/>
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon" href="<?= BASE_URL.'apple-touch-icon.png' ?>"/>
     </head>
     <body>
         <header class="header">
