c81c8e3a1bb4e1941f196f36e2beac30f077bd86
Allen Garvey <allen.garvey@gmail.com>
2018-05-23 17:39:16 -0400

    Show animation when color picker is opened and the overlay is clicked

--- js_src/app/vues/color-picker-component.js
+++ js_src/app/vues/color-picker-component.js
@@ -9,6 +9,16 @@
             bubbleEvent: function(name, args){
                 this.$emit(name, args);
             },
+            getAttention: function(){
+                const container = this.$refs.colorPickerContainer;
+                container.scrollIntoView({behavior: 'smooth'})
+                container.classList.remove('attention-animation');
+                //from https://css-tricks.com/restart-css-animation/
+                //we need to do this to retrigger the animation
+                void container.offsetWidth;
+                container.classList.add('attention-animation');
+            },
+
         },
     });
     
--- templates/index/vue_components/color-picker-component.php
+++ templates/index/vue_components/color-picker-component.php
@@ -1,6 +1,6 @@
 <div v-scroll-into-view>
-    <div class="color-picker-container">
+    <div ref="colorPickerContainer" class="color-picker-container">
         <photoshop-picker :value="selectedColor" @input="bubbleEvent('input', $event)" @ok="bubbleEvent('ok', $event)" @cancel="bubbleEvent('cancel', $event)" />
     </div>
-    <div class="color-picker-overlay"></div>
+    <div class="color-picker-overlay" @click="getAttention"></div>
 </div>
