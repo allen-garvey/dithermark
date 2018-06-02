985dda8877b38169ff25ab490ac3ebfb791582f8
Allen Garvey <allen.garvey@gmail.com>
2018-06-02 13:59:14 -0400

    Add more bilateral filter values

--- js_src/app/image-filters-model.js
+++ js_src/app/image-filters-model.js
@@ -22,7 +22,7 @@ App.ImageFiltersModel = (function(){
         //pixel values for smoothing filter
         smoothingValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16],
         //-1 means filter disabled
-        bilateralFilterValues:  [-1, 0, 5, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 35, 40],
+        bilateralFilterValues:  [-1, 0, 3, 4, 5, 7, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 35, 40, 50, 60],
         imageFilterValues,
         //contrast highest value is 300%
         contrastFilterValues: imageFilterValues.filter((value)=>{ return value >= 100; }),
