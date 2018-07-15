ac744a65adc7a4ede3052e23e5dac02ae898c98f
Allen Garvey <allen.garvey@gmail.com>
2018-07-15 12:54:31 -0400

    Change fragment shader rgb/hsl conversion to not require intermediate hsv conversion

--- templates/index/webgl_shaders/fragment/shared/hsl-functions.php
+++ templates/index/webgl_shaders/fragment/shared/hsl-functions.php
@@ -7,56 +7,42 @@
 </script>
 
 <script type="webgl/fragment-shader-function" id="webgl-hsl-functions">
-    <?php //from: https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
+    <?php 
+        //from: http://www.chilliant.com/rgb2hsv.html
+        //also relevant: https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
         //and http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
      ?>
-    vec3 rgb2hsv(vec3 c){
-        vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-        <?php 
-            //vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-            //vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-        ?>
-        vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
-        vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
-
-        float d = q.x - min(q.w, q.y);
-        float e = 1.0e-10;
-        return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    vec3 RGBtoHCV(vec3 rgb){
+        float epsilon = 1.0e-10;
+        // Based on work by Sam Hocevar and Emil Persson
+        vec4 P = (rgb.g < rgb.b) ? vec4(rgb.bg, -1.0, 2.0/3.0) : vec4(rgb.gb, 0.0, -1.0/3.0);
+        vec4 Q = (rgb.r < P.x) ? vec4(P.xyw, rgb.r) : vec4(rgb.r, P.yzx);
+        float C = Q.x - min(Q.w, Q.y);
+        float H = abs((Q.w - Q.y) / (6.0 * C + epsilon) + Q.z);
+        return vec3(H, C, Q.x);
     }
 
-    vec3 hsv2rgb(vec3 c){
-        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-    }
-
-    <?php //from: https://codeitdown.com/hsl-hsb-hsv-color/ ?>
-    vec3 hsv2hsl(vec3 pixel){
-        float l = 0.5 * pixel.b * (2.0 - pixel.g);
-        <?php //so no division by 0 ?>
-        float s = l == 1.0 || l == 0.0 ? 0.0 : pixel.g * pixel.b / (1.0 - abs(2.0 * l - 1.0));
-        return vec3(pixel.r, s, l);
+    vec3 HUEtoRGB(float hue){
+        float r = abs(hue * 6.0 - 3.0) - 1.0;
+        float g = 2.0 - abs(hue * 6.0 - 2.0);
+        float b = 2.0 - abs(hue * 6.0 - 4.0);
+        <?php //saturate function is the same as clamping between 0.0, and 1.0 ?>
+        return clamp(vec3(r, g, b), 0.0, 1.0);
     }
 
     vec3 rgb2hsl(vec3 pixel){
-        return hsv2hsl(rgb2hsv(pixel));
+        float epsilon = 1.0e-10;
+        vec3 hcv = RGBtoHCV(pixel);
+        float l = hcv.z - hcv.y * 0.5;
+        float s = hcv.y / (1.0 - abs(l * 2.0 - 1.0) + epsilon);
+        return vec3(hcv.x, s, l);
     }
 
-    <?php //from: https://codeitdown.com/hsl-hsb-hsv-color/ ?>
-    vec3 hsl2hsv(vec3 pixel){
-        float b = (2.0 * pixel.b + pixel.g * (1.0 - abs(2.0 * pixel.b - 1.0))) / 2.0;
-        float s = 2.0 * (b - pixel.b) / b;
-        return vec3(pixel.r, s, b);
-    }
     <?php //conversion does not work correctly for #000 or #fff, so added special case ?>
     vec3 hsl2rgb(vec3 pixel){
-        if(pixel.b == 1.0){
-            return vec3(1.0);
-        }
-        else if(pixel.b == 0.0){
-            return vec3(0.0);
-        }
-        return hsv2rgb(hsl2hsv(pixel));
+        vec3 rgb = HUEtoRGB(pixel.x);
+        float c = (1.0 - abs(2.0 * pixel.z - 1.0)) * pixel.y;
+        return (rgb - 0.5) * c + pixel.z;
     }
     
     float hue_distance(float hue1, float hue2){
@@ -66,8 +52,7 @@
     }
     
     float hue(vec3 pixel){
-        vec3 pixelHsv = rgb2hsv(pixel);
-        return pixelHsv.r;
+        return rgb2hsl(pixel).r;
     }
 
     float pixel_luma(vec3 pixel){
