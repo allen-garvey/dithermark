996d228e5cb5b3602109567a5ec6f83d9b47f3ca
Allen Garvey <allen.garvey@gmail.com>
2018-07-20 15:36:11 -0400

    Update neuquant to ES6 syntax, and remove unused functions and variables

--- js_src/image_dithering_worker/optimize-palette-neu-quant.js
+++ js_src/image_dithering_worker/optimize-palette-neu-quant.js
@@ -24,45 +24,43 @@
  */
 App.OptimizePaletteNeuQuant = (function(){
     
-    var ncycles = 100; // number of learning cycles
-    var netsize = 256; // number of colors used
-    var maxnetpos = netsize - 1;
+    const ncycles = 100; // number of learning cycles
+    const netsize = 256; // number of colors used
+    const maxnetpos = netsize - 1;
 
     // defs for freq and bias
-    var netbiasshift = 4; // bias for colour values
-    var intbiasshift = 16; // bias for fractions
-    var intbias = (1 << intbiasshift);
-    var gammashift = 10;
-    var gamma = (1 << gammashift);
-    var betashift = 10;
-    var beta = (intbias >> betashift); /* beta = 1/1024 */
-    var betagamma = (intbias << (gammashift - betashift));
+    const netbiasshift = 4; // bias for colour values
+    const intbiasshift = 16; // bias for fractions
+    const intbias = (1 << intbiasshift);
+    const gammashift = 10;
+    const betashift = 10;
+    const beta = (intbias >> betashift); /* beta = 1/1024 */
+    const betagamma = (intbias << (gammashift - betashift));
 
     // defs for decreasing radius factor
-    var initrad = (netsize >> 3); // for 256 cols, radius starts
-    var radiusbiasshift = 6; // at 32.0 biased by 6 bits
-    var radiusbias = (1 << radiusbiasshift);
-    var initradius = (initrad * radiusbias); //and decreases by a
-    var radiusdec = 30; // factor of 1/30 each cycle
+    const initrad = (netsize >> 3); // for 256 cols, radius starts
+    const radiusbiasshift = 6; // at 32.0 biased by 6 bits
+    const radiusbias = (1 << radiusbiasshift);
+    const initradius = (initrad * radiusbias); //and decreases by a
+    const radiusdec = 30; // factor of 1/30 each cycle
 
     // defs for decreasing alpha factor
-    var alphabiasshift = 10; // alpha starts at 1.0
-    var initalpha = (1 << alphabiasshift);
-    var alphadec; // biased by 10 bits
+    const alphabiasshift = 10; // alpha starts at 1.0
+    const initalpha = (1 << alphabiasshift);
 
     /* radbias and alpharadbias used for radpower calculation */
-    var radbiasshift = 8;
-    var radbias = (1 << radbiasshift);
-    var alpharadbshift = (alphabiasshift + radbiasshift);
-    var alpharadbias = (1 << alpharadbshift);
+    const radbiasshift = 8;
+    const radbias = (1 << radbiasshift);
+    const alpharadbshift = (alphabiasshift + radbiasshift);
+    const alpharadbias = (1 << alpharadbshift);
 
     // four primes near 500 - assume no image has a length so large that it is
     // divisible by all four primes
-    var prime1 = 499;
-    var prime2 = 491;
-    var prime3 = 487;
-    var prime4 = 503;
-    var minpicturebytes = (3 * prime4);
+    const prime1 = 499;
+    const prime2 = 491;
+    const prime3 = 487;
+    const prime4 = 503;
+    const minpicturebytes = (3 * prime4);
 
     /*
     Constructor: NeuQuant
@@ -74,30 +72,28 @@ App.OptimizePaletteNeuQuant = (function(){
     >
     */
     function NeuQuant(pixels, samplefac) {
-        var network; // int[netsize][4]
-        var netindex; // for network lookup - really 256
+        let network; // int[netsize][4]
+        let netindex; // for network lookup - really 256
 
         // bias and freq arrays for learning
-        var bias;
-        var freq;
-        var radpower;
+        let bias;
+        let freq;
+        let radpower;
 
         /*
             Private Method: init
             sets up arrays
         */
-        function init() {
+        function init(){
             network = [];
             netindex = new Int32Array(256);
             bias = new Int32Array(netsize);
             freq = new Int32Array(netsize);
             radpower = new Int32Array(netsize >> 3);
 
-            var i, v;
-            for (i = 0; i < netsize; i++) {
-                v = (i << (netbiasshift + 8)) / netsize;
+            for(let i = 0; i < netsize; i++){
+                const v = (i << (netbiasshift + 8)) / netsize;
                 network[i] = new Float64Array([v, v, v, 0]);
-                //network[i] = [v, v, v, 0]
                 freq[i] = intbias / netsize;
                 bias[i] = 0;
             }
@@ -107,8 +103,8 @@ App.OptimizePaletteNeuQuant = (function(){
             Private Method: unbiasnet
             unbiases network to give byte values 0..255 and record position i to prepare for sort
         */
-        function unbiasnet() {
-            for (var i = 0; i < netsize; i++) {
+        function unbiasnet(){
+            for(let i = 0; i < netsize; i++){
                 network[i][0] >>= netbiasshift;
                 network[i][1] >>= netbiasshift;
                 network[i][2] >>= netbiasshift;
@@ -120,7 +116,7 @@ App.OptimizePaletteNeuQuant = (function(){
             Private Method: altersingle
             moves neuron *i* towards biased (b,g,r) by factor *alpha*
         */
-        function altersingle(alpha, i, b, g, r) {
+        function altersingle(alpha, i, b, g, r){
             network[i][0] -= (alpha * (network[i][0] - b)) / initalpha;
             network[i][1] -= (alpha * (network[i][1] - g)) / initalpha;
             network[i][2] -= (alpha * (network[i][2] - r)) / initalpha;
@@ -131,26 +127,25 @@ App.OptimizePaletteNeuQuant = (function(){
             moves neurons in *radius* around index *i* towards biased (b,g,r) by factor *alpha*
         */
         function alterneigh(radius, i, b, g, r) {
-            var lo = Math.abs(i - radius);
-            var hi = Math.min(i + radius, netsize);
+            const lo = Math.abs(i - radius);
+            const hi = Math.min(i + radius, netsize);
 
-            var j = i + 1;
-            var k = i - 1;
-            var m = 1;
+            let j = i + 1;
+            let k = i - 1;
+            let m = 1;
 
-            var p, a;
-            while ((j < hi) || (k > lo)) {
-                a = radpower[m++];
+            while((j < hi) || (k > lo)){
+                const a = radpower[m++];
 
                 if (j < hi) {
-                    p = network[j++];
+                    const p = network[j++];
                     p[0] -= (a * (p[0] - b)) / alpharadbias;
                     p[1] -= (a * (p[1] - g)) / alpharadbias;
                     p[2] -= (a * (p[2] - r)) / alpharadbias;
                 }
 
-                if (k > lo) {
-                    p = network[k--];
+                if(k > lo){
+                    const p = network[k--];
                     p[0] -= (a * (p[0] - b)) / alpharadbias;
                     p[1] -= (a * (p[1] - g)) / alpharadbias;
                     p[2] -= (a * (p[2] - r)) / alpharadbias;
@@ -170,28 +165,27 @@ App.OptimizePaletteNeuQuant = (function(){
             bias[i] = gamma * ((1 / netsize) - freq[i])
             */
 
-            var bestd = ~(1 << 31);
-            var bestbiasd = bestd;
-            var bestpos = -1;
-            var bestbiaspos = bestpos;
+            let bestd = ~(1 << 31);
+            let bestbiasd = bestd;
+            let bestpos = -1;
+            let bestbiaspos = bestpos;
 
-            var i, n, dist, biasdist, betafreq;
-            for (i = 0; i < netsize; i++) {
-                n = network[i];
+            for(let i = 0; i < netsize; i++){
+                const n = network[i];
 
-                dist = Math.abs(n[0] - b) + Math.abs(n[1] - g) + Math.abs(n[2] - r);
-                if (dist < bestd) {
+                const dist = Math.abs(n[0] - b) + Math.abs(n[1] - g) + Math.abs(n[2] - r);
+                if(dist < bestd){
                     bestd = dist;
                     bestpos = i;
                 }
 
-                biasdist = dist - ((bias[i]) >> (intbiasshift - netbiasshift));
-                if (biasdist < bestbiasd) {
+                const biasdist = dist - ((bias[i]) >> (intbiasshift - netbiasshift));
+                if(biasdist < bestbiasd){
                     bestbiasd = biasdist;
                     bestbiaspos = i;
                 }
 
-                betafreq = (freq[i] >> betashift);
+                const betafreq = (freq[i] >> betashift);
                 freq[i] -= betafreq;
                 bias[i] += (betafreq << gammashift);
             }
@@ -207,32 +201,34 @@ App.OptimizePaletteNeuQuant = (function(){
             sorts network and builds netindex[0..255]
         */
         function inxbuild() {
-            var i, j, p, q, smallpos, smallval, previouscol = 0, startpos = 0;
-            for (i = 0; i < netsize; i++) {
-                p = network[i];
-                smallpos = i;
-                smallval = p[1]; // index on g
+            let previouscol = 0;
+            let startpos = 0;
+            for (let i = 0; i < netsize; i++) {
+                const p = network[i];
+                let smallpos = i;
+                let smallval = p[1]; // index on g
                 // find smallest in i..netsize-1
-                for (j = i + 1; j < netsize; j++) {
-                    q = network[j];
+                for (let j = i + 1; j < netsize; j++) {
+                    const q = network[j];
                     if (q[1] < smallval) { // index on g
                         smallpos = j;
                         smallval = q[1]; // index on g
                     }
                 }
-                q = network[smallpos];
-                // swap p (i) and q (smallpos) entries
-                if (i != smallpos) {
-                    j = q[0];   q[0] = p[0];   p[0] = j;
-                    j = q[1];   q[1] = p[1];   p[1] = j;
-                    j = q[2];   q[2] = p[2];   p[2] = j;
-                    j = q[3];   q[3] = p[3];   p[3] = j;
+                const q = network[smallpos];
+
+                if(i !== smallpos){
+                    // swap p (i) and q (smallpos) entries
+                    for(let j=0;j<4;j++){
+                        const temp = q[j];
+                        q[j] = p[j];
+                        p[j] = temp;
+                    }
                 }
                 // smallval entry is now in position i
-
-                if (smallval != previouscol){
+                if(smallval !== previouscol){
                     netindex[previouscol] = (startpos + i) >> 1;
-                    for (j = previouscol + 1; j < smallval; j++){
+                    for(let j = previouscol + 1; j < smallval; j++){
                         netindex[j] = i;
                     }
                     previouscol = smallval;
@@ -240,87 +236,9 @@ App.OptimizePaletteNeuQuant = (function(){
                 }
             }
             netindex[previouscol] = (startpos + maxnetpos) >> 1;
-            for (j = previouscol + 1; j < 256; j++){
+            for(let j = previouscol + 1; j < 256; j++){
                 netindex[j] = maxnetpos; // really 256
             }        
-        }
-
-        /*
-            Private Method: inxsearch
-            searches for BGR values 0..255 and returns a color index
-        */
-        function inxsearch(b, g, r) {
-            var a, p, dist;
-
-            var bestd = 1000; // biggest possible dist is 256*3
-            var best = -1;
-
-            var i = netindex[g]; // index on g
-            var j = i - 1; // start at netindex[g] and work outwards
-
-            while ((i < netsize) || (j >= 0)) {
-                if (i < netsize) {
-                    p = network[i];
-                    dist = p[1] - g; // inx key
-                    if (dist >= bestd) {
-                        i = netsize; // stop iter
-                    }
-                    else {
-                        i++;
-                        if (dist < 0) {
-                            dist = -dist;
-                        }
-                        a = p[0] - b; 
-                        if (a < 0) {
-                            a = -a;
-                        }
-                        dist += a;
-                        if (dist < bestd) {
-                            a = p[2] - r; 
-                            if (a < 0){
-                                a = -a;
-                            }
-                            dist += a;
-                            if (dist < bestd) {
-                                bestd = dist;
-                                best = p[3];
-                            }
-                        }
-                    }
-                }
-                if (j >= 0) {
-                    p = network[j];
-                    dist = g - p[1]; // inx key - reverse dif
-                    if (dist >= bestd) {
-                        // stop iter
-                        j = -1;
-                    }
-                    else {
-                        j--;
-                        if (dist < 0) {
-                            dist = -dist;
-                        }
-                        a = p[0] - b; 
-                        if (a < 0){ 
-                            a = -a;
-                        }
-                        dist += a;
-                        if (dist < bestd) {
-                            a = p[2] - r; 
-                            if (a < 0){ 
-                                a = -a;
-                            }
-                            dist += a;
-                            if (dist < bestd) {
-                                bestd = dist;
-                                best = p[3];
-                            }
-                        }
-                    }
-                }
-            }
-
-            return best;
         }
 
         /*
@@ -328,49 +246,47 @@ App.OptimizePaletteNeuQuant = (function(){
             "Main Learning Loop"
         */
         function learn() {
-            var i;
+            const lengthcount = pixels.length;
+            const alphadec = 30 + ((samplefac - 1) / 3);
+            const samplepixels = lengthcount / (3 * samplefac);
+            let delta = ~~(samplepixels / ncycles);
+            let alpha = initalpha;
+            let radius = initradius;
 
-            var lengthcount = pixels.length;
-            var alphadec = 30 + ((samplefac - 1) / 3);
-            var samplepixels = lengthcount / (3 * samplefac);
-            var delta = ~~(samplepixels / ncycles);
-            var alpha = initalpha;
-            var radius = initradius;
-
-            var rad = radius >> radiusbiasshift;
-
-            if (rad <= 1){ 
+            let rad = radius >> radiusbiasshift;
+            if(rad <= 1){ 
                 rad = 0;
             }
-            for (i = 0; i < rad; i++){
+            for(let i = 0; i < rad; i++){
                 radpower[i] = alpha * (((rad * rad - i * i) * radbias) / (rad * rad));
             }
             
 
-            var step;
-            if (lengthcount < minpicturebytes) {
-            samplefac = 1;
-            step = 3;
-            } else if ((lengthcount % prime1) !== 0) {
-            step = 3 * prime1;
-            } else if ((lengthcount % prime2) !== 0) {
-            step = 3 * prime2;
-            } else if ((lengthcount % prime3) !== 0)  {
-            step = 3 * prime3;
-            } else {
-            step = 3 * prime4;
+            let step;
+            if(lengthcount < minpicturebytes){
+                samplefac = 1;
+                step = 3;
+            } 
+            else if ((lengthcount % prime1) !== 0) {
+                step = 3 * prime1;
+            } 
+            else if ((lengthcount % prime2) !== 0) {
+                step = 3 * prime2;
+            } 
+            else if ((lengthcount % prime3) !== 0)  {
+                step = 3 * prime3;
+            } 
+            else {
+                step = 3 * prime4;
             }
-
-            var b, g, r, j;
-            var pix = 0; // current pixel
-
-            i = 0;
+            
+            let pix = 0; // current pixel
+            let i = 0;
             while (i < samplepixels) {
-                b = (pixels[pix] & 0xff) << netbiasshift;
-                g = (pixels[pix + 1] & 0xff) << netbiasshift;
-                r = (pixels[pix + 2] & 0xff) << netbiasshift;
-
-                j = contest(b, g, r);
+                const b = (pixels[pix] & 0xff) << netbiasshift;
+                const g = (pixels[pix + 1] & 0xff) << netbiasshift;
+                const r = (pixels[pix + 2] & 0xff) << netbiasshift;
+                const j = contest(b, g, r);
 
                 altersingle(alpha, j, b, g, r);
                 if (rad !== 0) alterneigh(rad, j, b, g, r); // alter neighbours
@@ -379,15 +295,14 @@ App.OptimizePaletteNeuQuant = (function(){
                 if (pix >= lengthcount) pix -= lengthcount;
 
                 i++;
-
-                if (delta === 0) delta = 1;
-                if (i % delta === 0) {
+                delta = delta === 0 ? 1 : delta;
+                if(i % delta === 0){
                     alpha -= alpha / alphadec;
                     radius -= radius / radiusdec;
                     rad = radius >> radiusbiasshift;
+                    rad = rad <= 1 ? 0 : rad;
 
-                    if (rad <= 1) rad = 0;
-                    for (j = 0; j < rad; j++){
+                    for(let j = 0; j < rad; j++){
                         radpower[j] = alpha * (((rad * rad - j * j) * radbias) / (rad * rad));
                     }
                 }
@@ -401,13 +316,12 @@ App.OptimizePaletteNeuQuant = (function(){
             3. removes misconceptions
             4. builds colorindex
         */
-        function buildColormap() {
+        this.buildColormap = function(){
             init();
             learn();
             unbiasnet();
             inxbuild();
-        }
-        this.buildColormap = buildColormap;
+        };
 
         /*
             Method: getColormap
@@ -417,33 +331,23 @@ App.OptimizePaletteNeuQuant = (function(){
             > [r, g, b, r, g, b, r, g, b, ..]
             >
         */
-        function getColormap() {
-            var map = new Uint8Array(netsize*3);
-            var index = [];
+        this.getColormap = function() {
+            const map = new Uint8Array(netsize*3);
+            const index = [];
 
-            for (var i = 0; i < netsize; i++){
+            for(let i = 0; i < netsize; i++){
                 index[network[i][3]] = i;
             }
 
-            var k = 0;
-            for (var l = 0; l < netsize; l++) {
-                var j = index[l];
+            for(let i = 0, k=0; i < netsize; i++){
+                const j = index[i];
                 map[k++] = (network[j][0]);
                 map[k++] = (network[j][1]);
                 map[k++] = (network[j][2]);
             }
             return map;
-        }
-        this.getColormap = getColormap;
-
-        /*
-            Method: lookupRGB
-            looks for the closest *r*, *g*, *b* color in the map and
-            returns its index
-        */
-        this.lookupRGB = inxsearch;
+        };
     }
-    
     
     function neuQuant(pixels, numColors, colorQuantization, _imageWidth, _imageHeight, progressCallback){
         const quantizer = new NeuQuant(pixels, colorQuantization.sample);
