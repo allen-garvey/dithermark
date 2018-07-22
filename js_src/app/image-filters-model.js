c64f2ef3a58be13293d83390d1482ea60312245a
Allen Garvey <allen.garvey@gmail.com>
2018-07-22 16:38:47 -0400

    Adjust outline filter opacity values for a more gradual transition

--- js_src/app/image-filters-model.js
+++ js_src/app/image-filters-model.js
@@ -58,7 +58,7 @@ App.ImageFiltersModel = (function(ArrayUtil){
     }
 
     function outlineOpacities(){
-        return [1, 0.9, 0.8, 0.70, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1];
+        return [1, 0.75, 0.6, 0.5, 0.4, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.03];
     }
 
     //used for blending fixed outline color with dithered output
