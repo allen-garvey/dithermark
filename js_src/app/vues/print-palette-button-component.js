8582749db916d4252f66a589966ae16ec19c378b
Allen Garvey <allen.garvey@gmail.com>
2018-08-15 11:37:22 -0400

    Edit print palette button to also copy palette to clipboard

--- js_src/app/vues/print-palette-button-component.js
+++ js_src/app/vues/print-palette-button-component.js
@@ -5,7 +5,15 @@
         methods: {
             printPalette: function(){
                 //used to simplify palette creation
-                console.log(JSON.stringify(this.colors).replace(/"/g, '\'').replace(/,/g, ', '));
+                const paletteString = JSON.stringify(this.colors).replace(/"/g, '\'').replace(/,/g, ', ');
+                console.log(paletteString);
+
+                //copies palette to clipboard
+                //will only work on chrome when served over https or localhost
+                //note this happens asynchronously
+                if(navigator.clipboard && navigator.clipboard.writeText){
+                    navigator.clipboard.writeText(paletteString);
+                }
             },
         },
     });
