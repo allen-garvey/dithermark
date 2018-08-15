97312bdf0312961a4a65c8447f09fc23c82eb8f4
Allen Garvey <allen.garvey@gmail.com>
2018-08-15 15:43:50 -0400

    Edit print palette component copy to clipboard to support older clipboard api as well so it works in other browsers

--- js_src/app/vues/print-palette-button-component.js
+++ js_src/app/vues/print-palette-button-component.js
@@ -7,12 +7,20 @@
                 //used to simplify palette creation
                 const paletteString = JSON.stringify(this.colors).replace(/"/g, '\'').replace(/,/g, ', ');
                 console.log(paletteString);
-
-                //copies palette to clipboard
+                
+                //copy palette to clipboard
                 //will only work on chrome when served over https or localhost
                 //note this happens asynchronously
                 if(navigator.clipboard && navigator.clipboard.writeText){
                     navigator.clipboard.writeText(paletteString);
+                }
+                //copy using older clipboard api
+                //from: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+                else{
+                    const textarea = this.$refs.copyTextarea;
+                    textarea.value = paletteString;
+                    textarea.select();
+                    document.execCommand('copy');
                 }
             },
         },
