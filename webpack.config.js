8678e19be8e06330de45ef274b334fd39057737e
Allen Garvey <allen.garvey@gmail.com>
2018-08-26 14:14:52 -0400

    Remove commented out vue-infinite-scroll resolve alias in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
     resolve: {
         alias: {
             'vue': path.resolve(__dirname, 'node_modules/vue/dist/vue.js'),
-            // 'vue-infinite-scroll': path.resolve(__dirname, 'node_modules/vue-infinite-scroll/vue-infinite-scroll.js'),
             'app-performance-timer': path.resolve(__dirname, 'js_src/shared/timer.js'),
         }
     },
