d7daaccd76b54ff7ef1313d4b3bfeeffa3e95bf4
Allen Garvey <allen.garvey@gmail.com>
2018-08-26 12:24:51 -0400

    Remove cycle property list php partial, since this is now a vue component

--- templates/index/partials/cycle-property-list.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <button class="cycle-property-list-button btn btn-default btn-xs" title="Previous <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsPreviousArgs; ?>)"><</button>
-    <button class="cycle-property-list-button btn btn-default btn-xs" title="Next <?= $modelTitle; ?>" @click="cyclePropertyList(<?= $cyclePropertyListJsNextArgs; ?>)">></button>
-</div>
