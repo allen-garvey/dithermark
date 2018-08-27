2f6187f1d98f6dc77014db5fa5bfdd84b7d72da6
Allen Garvey <allen.garvey@gmail.com>
2018-08-26 23:31:01 -0400

    Remove js minification stuff from gulp, as it is now handled by webpack

--- gulp-config.js
+++ gulp-config.js
@@ -7,28 +7,6 @@ config.SOURCE_DIR = path.join(__dirname, '/');
 config.DEST_DIR = path.join(__dirname, 'public_html');
 
 /*
-* JS Configuration 
-* for google closure compiler https://github.com/google/closure-compiler-js
-*/
-config.js = {};
-config.js.SOURCE_DIR = path.join(config.DEST_DIR, 'js/');
-config.js.DEST_DIR = config.js.SOURCE_DIR;
-const WORKER_NAME = 'worker';
-config.js.worker_src_name = `${WORKER_NAME}.js`;
-config.js.worker_dest_name = `${WORKER_NAME}.min.js`;
-const APP_NAME = 'app';
-config.js.app_src_name = `${APP_NAME}.js`;
-config.js.app_dest_name = `${APP_NAME}.min.js`;
-const JS_COMPILER_OPTIONS = {
-  compilationLevel: 'SIMPLE',
-  warningLevel: 'DEFAULT',
-  languageIn: 'ES6',
-  languageOut: 'ES6',
-};
-config.js.worker_options = Object.assign({jsOutputFile: config.js.worker_dest_name}, JS_COMPILER_OPTIONS);
-config.js.app_options = Object.assign({jsOutputFile: config.js.app_dest_name}, JS_COMPILER_OPTIONS);
-
-/*
 * Sass/Styles configuration
 */
 config.styles = {};
--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,8 @@
 const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const compiler = require('google-closure-compiler-js').gulp();
 
 const config = require(path.join(__dirname, 'gulp-config.js'));
-
-/*
-* JS Tasks
-*/
-
-gulp.task('minifyApp', function(){
-    return gulp.src(path.join(config.js.SOURCE_DIR, config.js.app_src_name))
-        .pipe(compiler(config.js.app_options))
-        .pipe(gulp.dest(config.js.DEST_DIR));
-});
-
-gulp.task('minifyWorker', function(){
-    return gulp.src(path.join(config.js.SOURCE_DIR, config.js.worker_src_name))
-        .pipe(compiler(config.js.worker_options))
-        .pipe(gulp.dest(config.js.DEST_DIR));
-});
 
 /*
 * Sass/Styles Tasks
@@ -47,5 +30,4 @@ gulp.task('watchSass', function(){
 //have to put watchSass sass task dependency here, since if we put it directly on the watchSass task, it will not watch, instead just run once
 gulp.task('watch', gulp.series(['sass', 'watchSass']));
 gulp.task('build', gulp.parallel(['sass']));
-gulp.task('release', gulp.parallel(['sass', 'minifyApp', 'minifyWorker']));
 gulp.task('default', gulp.parallel(['build']));
