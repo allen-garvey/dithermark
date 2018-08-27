d34da2dc7716c02efcfc8a4bded80595b0d76a36
Allen Garvey <allen.garvey@gmail.com>
2018-08-27 12:37:31 -0400

    Add dummy vue component for components that are not included in production build

---
+++ js_src/app/vues/dummy.vue
@@ -0,0 +1,9 @@
+<template>
+    <div v-if="false"></div>
+</template>
+<script>
+//this is an empty component for components that should not appear in release builds (print palette button, texture combine component)
+export default {
+    name: 'dummy'
+};
+</script>
