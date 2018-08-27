97c036327903c50f46d7bf0e047c2f82fc53b7e5
Allen Garvey <allen.garvey@gmail.com>
2018-08-26 23:34:52 -0400

    Change name of js bundle output file for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const config = require('./webpack.config.js');
 
-config.mode = "production";
-
+config.mode = 'production';
+config.output.filename = 'bundle.min.js';
 config.resolve.alias['app-performance-timer'] = path.resolve(__dirname, 'js_src/shared/timer-dummy.js');
 
 module.exports = config;
