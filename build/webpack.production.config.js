f64dfcc638711205b6bfcc372a81540af490c53b
Allen Garvey <allen.garvey@gmail.com>
2018-08-28 12:03:25 -0400

    Add custom options for uglify js plugin for webpack production builds

--- build/webpack.production.config.js
+++ build/webpack.production.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const config = require('./webpack.config.js');
 
 config.mode = 'production';
@@ -7,6 +8,25 @@ config.output.filename = 'bundle.min.js';
 config.resolve.alias['app-performance-timer'] = path.resolve(__dirname, '../js_src/shared/timer-dummy.js');
 config.resolve.alias['print-palette-button'] = path.resolve(__dirname, '../js_src/app/vues/dummy.vue');
 config.resolve.alias['texture-combine-component'] = path.resolve(__dirname, '../js_src/app/vues/dummy.vue');
+
+
+config.optimization = {
+    minimizer: [  
+        new UglifyJsPlugin({  
+            parallel: true,
+            cache: true,  
+            uglifyOptions: {  
+                compress: {  
+                    ecma: 8,
+                    warnings: true,
+                },
+                ecma: 8,
+                warn: true,
+                mangle: true,
+            },  
+        }),  
+    ],
+};
 
 
 module.exports = config;
