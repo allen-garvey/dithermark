d143cc7c3bcc796610ee2adbfa34c6d6ff6abfab
Allen Garvey <allengarvey@hotmail.com>
2022-03-06 07:52:24 -0500

    Fix code indentation in open tab component

--- js_src/app/vues/open-tab.vue
+++ js_src/app/vues/open-tab.vue
@@ -2,7 +2,13 @@
     <div class="controls-tab-container">
         <fieldset>
             <legend>Device</legend>
-            <button class="btn btn-primary" @click="openDeviceImage" title="Open local image file">Image file</button>
+            <button 
+                class="btn btn-primary" 
+                @click="openDeviceImage" 
+                title="Open local image file"
+            >
+                Image file
+            </button>
             <input 
                 type="file"
                 @change.prevent="onFileInputChange($event)"
@@ -12,8 +18,22 @@
         </fieldset>
         <fieldset>
             <legend>Web</legend>
-            <button class="btn btn-default" @click="showOpenImageUrlPrompt" :disabled="isCurrentlyLoadingImageUrl" title="Open image from Url">Image url</button>
-        <button class="btn btn-default" @click="openRandomImage" :disabled="isCurrentlyLoadingImageUrl" title="Open random image from Unsplash">Random image</button>
+            <button 
+                class="btn btn-default" 
+                @click="showOpenImageUrlPrompt" 
+                :disabled="isCurrentlyLoadingImageUrl" 
+                title="Open image from Url"
+            >
+                Image url
+            </button>
+            <button 
+                class="btn btn-default" 
+                @click="openRandomImage" 
+                :disabled="isCurrentlyLoadingImageUrl" 
+                title="Open random image from Unsplash"
+            >
+                Random image
+            </button>
         </fieldset>    
     </div>
 </template>
