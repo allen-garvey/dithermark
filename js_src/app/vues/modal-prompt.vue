c77b9bb3d953049333023808b96c371bb835f232
Allen Garvey <allengarvey@hotmail.com>
2022-03-11 08:16:13 -0500

    Add horizontal spacing between elements is modal prompt component

--- js_src/app/vues/modal-prompt.vue
+++ js_src/app/vues/modal-prompt.vue
@@ -4,19 +4,44 @@
         <div class="modal-container">
             <label>{{labelValue}}
                 <!-- //trigger needs to be keydown and not keyup, since otherwise it will be triggered if modal is opened via enter key on button -->
-                <input ref="input" v-focus class="modal-input" tabindex="1" :type="inputType" :placeholder="placeholder" v-model="inputValue" @keydown.enter="okAction" />
+                <input 
+                    ref="input" 
+                    v-focus class="modal-input" 
+                    tabindex="1" 
+                    :type="inputType" 
+                    :placeholder="placeholder" 
+                    v-model="inputValue"
+                    @keydown.enter="okAction"
+                    :class="$style.input" 
+                />
             </label>
             <div class="modal-buttons-container">
                 <button class="btn btn-default" tabindex="2" @click="cancelAction">Cancel</button>
-                <button class="btn btn-primary" tabindex="3" @click="okAction" :disabled="!inputValue">{{okButtonValue}}</button>
+                <button 
+                    class="btn btn-primary" 
+                    :class="$style.okButton"
+                    tabindex="3" 
+                    @click="okAction" 
+                    :disabled="!inputValue"
+                >
+                    {{okButtonValue}}
+                </button>
             </div>
         </div>
     </div>
 </template>
 
+<style lang="scss" module>
+    .input {
+        margin-left: 0.5rem;
+    }
+    .okButton {
+        margin-left: 0.5rem;
+    }
+</style>
+
 <script>
 import FocusDirective from './directives/focus.js';
-
 
 export default {
     directives: {
