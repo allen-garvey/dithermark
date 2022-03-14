efc4472d3e6f3b3a9a011fbcd9651c8380807b68
Allen Garvey <allengarvey@hotmail.com>
2022-03-14 14:45:26 -0400

    Refactor modal prompt styles to css component

--- js_src/app/vues/modal-prompt.vue
+++ js_src/app/vues/modal-prompt.vue
@@ -1,12 +1,11 @@
 <template>
-    <div v-if="showModal" @keyup.esc="cancelAction" class="modal-layout-container">
-        <div class="modal-overlay" @click="cancelAction"></div>
-        <div class="modal-container">
+    <div v-if="showModal" @keyup.esc="cancelAction" :class="$style.modal">
+        <div :class="$style.overlay" @click="cancelAction"></div>
+        <div :class="$style.contents">
             <label>{{labelValue}}
                 <!-- //trigger needs to be keydown and not keyup, since otherwise it will be triggered if modal is opened via enter key on button -->
-                <input 
-                    ref="input" 
-                    v-focus class="modal-input" 
+                <input  
+                    v-focus
                     tabindex="1" 
                     :type="inputType" 
                     :placeholder="placeholder" 
@@ -15,8 +14,14 @@
                     :class="$style.input" 
                 />
             </label>
-            <div class="modal-buttons-container">
-                <button class="btn btn-default" tabindex="2" @click="cancelAction">Cancel</button>
+            <div :class="$style.buttonsContainer">
+                <button 
+                    class="btn btn-default" 
+                    tabindex="2" 
+                    @click="cancelAction"
+                >
+                    Cancel
+                </button>
                 <button 
                     class="btn btn-primary" 
                     :class="$style.okButton"
@@ -32,12 +37,56 @@
 </template>
 
 <style lang="scss" module>
-    .input {
-        margin-left: 0.5rem;
-    }
-    .okButton {
-        margin-left: 0.5rem;
-    }
+.overlay, .modal{
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+
+.overlay{
+    background-color: black;
+    opacity: 0.7;
+    z-index: 999;
+}
+
+.modal{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+}
+
+.contents{
+    position: relative;
+    z-index: 1000;
+    background-color: var(--main-bg-color);
+    border: 1px solid var(--border-color);
+    box-sizing: border-box;
+    max-height: 100vh;
+    max-width: 100vw;
+    padding: 3.5em 3em;
+    border-radius: 4px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.buttonsContainer{
+    margin-top: 1.5em;
+}
+
+.input {
+    margin-left: 0.5rem;
+    width: 20em;
+    max-width: 100%;
+}
+
+.okButton {
+    margin-left: 0.5rem;
+}
 </style>
 
 <script>
