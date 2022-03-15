e8f7864ce62680bb3f3afd490c41d7a590fd562d
Allen Garvey <allengarvey@hotmail.com>
2022-03-15 08:18:57 -0400

    Refactor color input styles to css module

--- js_src/app/vues/color-input.vue
+++ js_src/app/vues/color-input.vue
@@ -1,11 +1,103 @@
 <template>
-    <div class="color-container" :draggable="draggableAttributeValue" @dragstart="optionalHandler($event, handleColorDragstart)" @dragover="optionalHandler($event, handleColorDragover)" @drop="handleColorDrop($event)" @dragend="optionalHandler($event, handleColorDragend)" :class="{'dragged': isBeingDragged, 'color-disabled': isDisabled, 'selected': isSelected}" :style="{'background-color': colorValue, 'color': textColor}">
+    <div 
+        :draggable="draggableAttributeValue" 
+        @dragstart="optionalHandler($event, handleColorDragstart)" 
+        @dragover="optionalHandler($event, handleColorDragover)" 
+        @drop="handleColorDrop($event)" 
+        @dragend="optionalHandler($event, handleColorDragend)" 
+        :class="{[$style.colorContainer]: true, [$style.dragged]: isBeingDragged, [$style.colorDisabled]: isDisabled, [$style.selected]: isSelected}" 
+        :style="{'background-color': colorValue, 'color': textColor}"
+    >
         <label :for="colorInputId">{{labelText}}</label>
-        <div class="faux-color-input-container">
-            <input :id="colorInputId" type="button" class="faux-color-input" @click="inputClicked" :style="{'background-color': colorValue}" :class="{disabled: isDisabled}"/>
+        <div :class="$style.fauxColorInputContainer">
+            <input 
+                :id="colorInputId" 
+                type="button" 
+                @click="inputClicked" 
+                :style="{'background-color': colorValue}" 
+                :class="{[$style.fauxColorInput]: true, disabled: isDisabled}"
+            />
         </div>
     </div>
 </template>
+
+<style lang="scss" module>
+
+.colorContainer{
+    //so currently selected color is not hidden by .color-picker-overlay
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-basis: 110px;
+    margin: 0 13px $colors_container_margin_bottom 0;
+    border: 1px solid var(--border-color);
+    border-radius: 5px;
+    padding: 4px;
+
+    //z-index should be higher than .color-picker-overlay
+    &.selected{
+        z-index: 3;
+    }
+    
+    &.dragged, &.colorDisabled{
+        opacity: 0.4;
+    }
+    label{
+        padding: 0;
+    }
+
+    &[draggable="true"]{
+        cursor: move;
+        .fauxColorInputContainer{
+            margin-right: 26px;
+        }
+    }
+    &:focus-within{
+        outline: 5px auto -webkit-focus-ring-color;
+        outline-offset: -2px;
+    }
+}
+
+//input[type="button"] that's meant to look and act like color input
+.fauxColorInputContainer{
+    border: 5px solid white;
+    border-radius: 6px;
+    display: inline-block;
+    &:focus-within{
+        outline: 5px auto -webkit-focus-ring-color;
+        border-color: $highlight_color;
+        outline-offset: -2px;
+    }
+}
+
+.fauxColorInput{
+    position: relative;
+    z-index: 1;
+    width: 46px;
+    height: 24px;
+    border: 1px solid black;
+    border-radius: 4px;
+    cursor: pointer;
+
+    &:before {
+        content: " ";
+        position: absolute;
+        z-index: -1;
+        top: 5px;
+        left: 5px;
+        right: 5px;
+        bottom: 5px;
+        border-radius: 4px;
+        border: 5px solid white;
+      }
+
+    &.disabled{
+        cursor: not-allowed;
+    }
+}
+
+</style>
 
 <script>
 //color picker component for color dither color list
