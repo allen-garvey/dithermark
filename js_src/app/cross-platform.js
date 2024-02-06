a9cf91f7e4df82549d8cf7c5a14318cec4b096ab
Allen Garvey <allengarvey@hotmail.com>
2024-02-06 14:01:24 -0500

    Fix missing export

--- js_src/app/cross-platform.js
+++ js_src/app/cross-platform.js
@@ -1,7 +1,7 @@
 // from: https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 // will be false positive on IE11 or if user agent is spoofed, but is not a big deal,
 // since IE11 is not supported anyway.
-function isiOs() {
+export function isiOs() {
     const iOsRegex = /iP(ad|hone|od)/;
     return (
         iOsRegex.test(navigator.userAgent) ||
