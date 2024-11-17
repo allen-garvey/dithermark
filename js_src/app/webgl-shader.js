6718309983f1d1d18f7503273d1f8f901d2db6d1
Allen Garvey <allengarvey@hotmail.com>
2024-11-17 12:35:34 -0500

    Change getting webgl shaders from dom to cache results

--- js_src/app/webgl-shader.js
+++ js_src/app/webgl-shader.js
@@ -1,32 +1,52 @@
 /*
-* Module for shared functionality for generating vertex and fragment shaders
-* used in both BW and Color webgl dithers
-*/
+ * Module for shared functionality for generating vertex and fragment shaders
+ * used in both BW and Color webgl dithers
+ */
 
-function shaderText(id){
-    if(!id){
-        return '';
+const shaderTextMap = new Map();
+
+/**
+ * @param {string} id
+ */
+function shaderText(id) {
+    if (shaderTextMap.has(id)) {
+        return shaderTextMap.get(id);
     }
-    return document.getElementById(id).textContent;
+
+    const shader = document.getElementById(id).textContent;
+    shaderTextMap.set(id, shader);
+
+    return shader;
 }
 
-function generateBitwiseFunctionsText(){
-    function generateOperator(functionName, operation){
-        return functionTemplate.replace('#{{functionName}}', functionName).replace('#{{operation}}', operation);
+function generateBitwiseFunctionsText() {
+    function generateOperator(functionName, operation) {
+        return functionTemplate
+            .replace('#{{functionName}}', functionName)
+            .replace('#{{operation}}', operation);
     }
     let functionTemplate = shaderText('webgl-bitwise-function-template');
-    return generateOperator('AND', 'bit1IsOdd && bit2IsOdd') + generateOperator('XOR', '(bit1IsOdd || bit2IsOdd) && !(bit1IsOdd && bit2IsOdd)');
+    return (
+        generateOperator('AND', 'bit1IsOdd && bit2IsOdd') +
+        generateOperator(
+            'XOR',
+            '(bit1IsOdd || bit2IsOdd) && !(bit1IsOdd && bit2IsOdd)'
+        )
+    );
 }
 
-
 export default {
-    shaderText: shaderText,
+    shaderText,
     vertexShaderText: shaderText('webgl-vertex-shader'),
     generateBitwiseFunctionsText: generateBitwiseFunctionsText,
     aDitherAdd1Return: 'aDitherMask3(x, y)',
-    aDitherAdd2Return: '(aDitherMask4(x, y, 0) + aDitherMask4(x, y, 1) + aDitherMask4(x, y, 2)) / 3.0',
-    aDitherAdd3Return: '(aDitherMask4(x, y, int(pixel.r * 3.)) + aDitherMask4(x, y, int(pixel.g * 3.)) + aDitherMask4(x, y, int(pixel.b * 3.))) / 3.0',
+    aDitherAdd2Return:
+        '(aDitherMask4(x, y, 0) + aDitherMask4(x, y, 1) + aDitherMask4(x, y, 2)) / 3.0',
+    aDitherAdd3Return:
+        '(aDitherMask4(x, y, int(pixel.r * 3.)) + aDitherMask4(x, y, int(pixel.g * 3.)) + aDitherMask4(x, y, int(pixel.b * 3.))) / 3.0',
     aDitherXor1Return: 'aDitherMask1(x, y)',
-    aDitherXor2Return: '(aDitherMask2(x, y, 0) + aDitherMask2(x, y, 1) + aDitherMask2(x, y, 2)) / 3.0',
-    aDitherXor3Return: '(aDitherMask2(x, y, int(pixel.r * 3.)) + aDitherMask2(x, y, int(pixel.g * 3.)) + aDitherMask2(x, y, int(pixel.b * 3.))) / 3.0',
+    aDitherXor2Return:
+        '(aDitherMask2(x, y, 0) + aDitherMask2(x, y, 1) + aDitherMask2(x, y, 2)) / 3.0',
+    aDitherXor3Return:
+        '(aDitherMask2(x, y, int(pixel.r * 3.)) + aDitherMask2(x, y, int(pixel.g * 3.)) + aDitherMask2(x, y, int(pixel.b * 3.))) / 3.0',
 };
