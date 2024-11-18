52c92f9d9ae7bb721adc2c6193dadae2a86f6086
Allen Garvey <allengarvey@hotmail.com>
2024-11-18 13:18:49 -0500

    Remove unused php constants and php build mode from makefile, as it is no longer used

--- inc/config.php
+++ inc/config.php
@@ -1,20 +1,4 @@
 <?php
-    //find out if we are running from makefile, or directly through fastcgi
-    if(!empty($argv)){
-        define('IS_FASTCGI', false);
-    }
-    else{
-        define('IS_FASTCGI', true);
-    }
-
-    //get build mode from command-line arguments
-    if(!IS_FASTCGI && !empty($argv) && count($argv) > 1 && $argv[1] === 'release'){
-        define('BUILD_MODE_RELEASE', true);
-    }
-    else{
-        define('BUILD_MODE_RELEASE', false);
-    }
-
     /*
     * File path constants
     */
@@ -28,16 +12,3 @@
     define('JS_VUES_PATH', JS_APP_PATH.'vues'.DIRECTORY_SEPARATOR);
     define('JS_SHARED_PATH', JS_SRC_PATH.'shared'.DIRECTORY_SEPARATOR);
     define('JS_WORKER_PATH', JS_SRC_PATH.'image_dithering_worker'.DIRECTORY_SEPARATOR);
-
-    /**
-     * URLs
-     * */
-    define('BASE_URL', '/');
-    define('JS_URL_BASE', BASE_URL.'assets/');
-
-    if(BUILD_MODE_RELEASE){
-        define('JS_BUNDLE_URL', JS_URL_BASE.'bundle.min.js');
-    }
-    else{
-        define('JS_BUNDLE_URL', JS_URL_BASE.'bundle.js');
-    }
