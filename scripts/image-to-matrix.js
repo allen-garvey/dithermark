dbe6814bc331e6ba1eeb996c0f36643b74554674
Allen Garvey <allengarvey@hotmail.com>
2024-11-23 12:18:20 -0500

    Fix bug in image to matrix script when image has no alpha channel

--- scripts/image-to-matrix.js
+++ scripts/image-to-matrix.js
@@ -10,20 +10,23 @@ const __dirname = path.dirname(__filename);
 
 const outputDir = path.join(__dirname, 'matrix-output');
 
-const imageName = 'heart-16x16.png';
+const imageName = 'stars-16x16.png';
 
 fs.mkdir(outputDir, { recursive: true })
     .then(() =>
-        sharp(path.join(__dirname, 'images', imageName)).raw().toBuffer()
+        sharp(path.join(__dirname, 'images', imageName))
+            .removeAlpha()
+            .raw()
+            .toBuffer()
     )
     .then((pixels) => {
         const pixelsLength = pixels.length;
-        const lightnessMatrix = new Uint8Array(pixelsLength / 4);
+        const lightnessMatrix = new Uint8Array(pixelsLength / 3);
         const lightnessDivisor = 256 / lightnessMatrix.length;
 
-        for (let i = 0, j = 0; i < pixelsLength; i += 4, j++) {
+        for (let i = 0, j = 0; i < pixelsLength; i += 3, j++) {
             lightnessMatrix[j] = Math.floor(
-                lightness(pixels.subarray(i, i + 4)) / lightnessDivisor
+                lightness(pixels.subarray(i, i + 3)) / lightnessDivisor
             );
         }
 
