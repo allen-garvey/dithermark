9ff26fbc3af58cfc97ba616c3c941471a2b81df6
Allen Garvey <allengarvey@hotmail.com>
2024-11-25 18:12:08 -0500

    Fix color simplex dither shader body using wrong coordinates

--- server/templates/shaders/fragment/dithers/color/base/simplex-color-dither-body.glsl
+++ server/templates/shaders/fragment/dithers/color/base/simplex-color-dither-body.glsl
@@ -1,2 +1,2 @@
-float simplexValue = u_dither_r_coefficient * (snoise(v_texcoord.xy) - 0.5);
+float simplexValue = u_dither_r_coefficient * (snoise(gl_FragCoord.xy) - 0.5);
 adjustedPixel = clamp(adjustedPixel + vec3(simplexValue), 0.0, 1.0);
