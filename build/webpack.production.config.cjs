dab71924721865c3747991b5e9abb4ec3f9a45f0
Allen Garvey <allengarvey@hotmail.com>
2024-11-29 13:47:27 -0500

    Remove terser and css minimizer package and configuration, since vue-color now uses scss

--- build/webpack.production.config.cjs
+++ build/webpack.production.config.cjs
@@ -1,6 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const config = require('./webpack.config.cjs');
 
 config.mode = 'production';
@@ -17,16 +15,5 @@ config.resolve.alias['texture-combine-component'] = path.resolve(
     __dirname,
     '../js/app/vues/dummy.vue'
 );
-
-config.optimization = {
-    minimizer: [
-        //since we are overrided minimizer for OptimizeCSSAssetsPlugin we need to manually add terser
-        new TerserPlugin({
-            parallel: true,
-        }),
-        //while the sass is already minified, we need this plugin so css extracted from dithermark-vue-color is minified too
-        new CssMinimizerPlugin(),
-    ],
-};
 
 module.exports = config;
