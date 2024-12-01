e29dc627ab3eaa13ac4c6113b80e91628ff91c6f
Allen Garvey <allengarvey@hotmail.com>
2024-12-01 11:32:06 -0500

    Remove unsplash access key from constants.js to prevent it from accidentally being included in bundle

--- scripts/unsplash-random-images.js
+++ scripts/unsplash-random-images.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 
-import { UNSPLASH_ACCESS_KEY } from '../constants.js';
+import { UNSPLASH_ACCESS_KEY } from '../unsplash-secrets.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
