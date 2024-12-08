1b4349b3aa5674731a4d0df413406d1125f18129
Allen Garvey <allen.garvey@gmail.com>
2024-12-08 12:36:28 -0500

    Refactor server webpack setup to be in webpack module

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ import {
     PUBLIC_HTML_DIR,
     ASSETS_DIR,
 } from '../build/webpack.config.js';
-import { createWebpackCompiler } from './webpack.js';
-
-const webpackCompiler = createWebpackCompiler(fs, getConfig());
+import { createWebpackCompiler, startWebpackCompiler } from './webpack.js';
 
 const app = express();
 const port = 3000;
@@ -61,32 +59,4 @@ app.listen(port, () => {
     );
 });
 
-/**
- *
- * @param {number} d
- * @returns {string}
- */
-const formatTimePart = d =>
-    d.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-
-webpackCompiler.watch({}, async (err, stats) => {
-    if (err) throw err;
-    const info = stats.toJson();
-    const now = new Date();
-
-    console.log(
-        `webpack compilation finished at ${formatTimePart(
-            now.getHours()
-        )}:${formatTimePart(now.getMinutes())}:${formatTimePart(
-            now.getSeconds()
-        )} in ${info.time / 1000}s`
-    );
-
-    if (stats.hasErrors()) {
-        console.error(info.errors);
-    }
-
-    if (stats.hasWarnings()) {
-        console.warn(info.warnings);
-    }
-});
+startWebpackCompiler(createWebpackCompiler(fs, getConfig()));
--- server/webpack.js
+++ server/webpack.js
@@ -7,3 +7,35 @@ export const createWebpackCompiler = (fs, config) => {
     compiler.outputFileSystem.join = path.join.bind(path);
     return compiler;
 };
+
+/**
+ *
+ * @param {number} d
+ * @returns {string}
+ */
+const formatTimePart = d =>
+    d.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+
+export const startWebpackCompiler = webpackCompiler => {
+    webpackCompiler.watch({}, async (err, stats) => {
+        if (err) throw err;
+        const info = stats.toJson();
+        const now = new Date();
+
+        console.log(
+            `webpack compilation finished at ${formatTimePart(
+                now.getHours()
+            )}:${formatTimePart(now.getMinutes())}:${formatTimePart(
+                now.getSeconds()
+            )} in ${info.time / 1000}s`
+        );
+
+        if (stats.hasErrors()) {
+            console.error(info.errors);
+        }
+
+        if (stats.hasWarnings()) {
+            console.warn(info.warnings);
+        }
+    });
+};
