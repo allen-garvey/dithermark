35870e5f665286ff4205bdb247fd8287ad5b0897
Allen Garvey <allen.garvey@gmail.com>
2024-12-08 13:44:55 -0500

    Refactor to simplify timer function

--- js/shared/timer.js
+++ js/shared/timer.js
@@ -1,28 +1,26 @@
 /**
  * Used for performance timing
  * if changing public functions, make sure to update timer-dummy.js for release builds
-*/
+ */
 
-function timeFunctionBase(functionToTime, done){
+/**
+ *
+ * @param {string} name
+ * @param {number} numPixels
+ * @param {Function} functionToTime
+ */
+function megapixelsPerSecond(name, numPixels, functionToTime) {
     const start = performance.now();
     functionToTime();
     const end = performance.now();
     const seconds = (end - start) / 1000;
-    done(seconds);
-}
-
-function timeFunctionMegapixels(name, numPixels, functionToTime){
-    timeFunctionBase(functionToTime, (seconds)=>{
-        console.log(megapixelsMessage(name, numPixels, seconds));
-    });
-}
-
-function megapixelsMessage(name, numPixels, seconds){
     const megapixels = numPixels / 1000000;
     const megapixelsPerSecond = megapixels / seconds;
-    return `${name}: ${seconds}s, ${megapixelsPerSecond.toFixed(2)} megapixels/s`;
+    console.log(
+        `${name}: ${seconds}s, ${megapixelsPerSecond.toFixed(2)} megapixels/s`
+    );
 }
 
 export default {
-    megapixelsPerSecond: timeFunctionMegapixels,
+    megapixelsPerSecond,
 };
