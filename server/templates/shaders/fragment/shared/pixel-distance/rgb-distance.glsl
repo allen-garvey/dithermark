df8494f59aee9a98d0b8e345b8aa3d744b67382b
Allen Garvey <allen.garvey@gmail.com>
2024-12-17 22:25:22 -0500

    Add note to quick distance function about why it is quicker

--- server/templates/shaders/fragment/shared/pixel-distance/rgb-distance.glsl
+++ server/templates/shaders/fragment/shared/pixel-distance/rgb-distance.glsl
@@ -1,3 +1,4 @@
+// a quicker version of built-in distance() function or pythagorean distance, since we don't need the square root since we are only using it for comparisons
 float quick_distance(vec3 pixel1, vec3 pixel2){
     vec3 distances = pixel1 - pixel2;
     return dot(vec3(1.0), distances * distances);
