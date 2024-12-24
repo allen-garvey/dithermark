8dae0e6008be6a38e6c394bb1ad9196ef7a2da27
Allen Garvey <allengarvey@hotmail.com>
2024-12-23 23:14:55 -0500

    Fix first frame of the video not displaying after video is loaded

--- js/app/vues/widgets/video-player.vue
+++ js/app/vues/widgets/video-player.vue
@@ -2,38 +2,17 @@
     <div :class="$style.container">
         <h4>Video Controls</h4>
         <p>{{ videoFile.name }}</p>
-        <input 
-            type="range" 
-            id="seek" 
-            min="0" 
-            :max="duration" 
-            step="0.5" 
-            :disabled="!isVideoLoaded" 
-            v-model.number="seekValue" 
-        />
-        <input 
-            type="number" 
-            min="0" 
-            :max="duration"  
-            step="1" 
-            v-model.number="seekValue" 
-            :disabled="!isVideoLoaded" 
-        />
-        <video
-            v-show="false"
-            ref="video"
-            @seeked="onSeeked"
-            @loadedmetadata="onMetadataLoaded"
-            @canplaythrough="onVideoReady"
-        >
+        <input type="range" id="seek" min="0" :max="duration" step="0.5" :disabled="!isVideoLoaded"
+            v-model.number="seekValue" />
+        <input type="number" min="0" :max="duration" step="1" v-model.number="seekValue" :disabled="!isVideoLoaded" />
+        <video v-show="false" ref="video" @seeked="onSeeked" @loadedmetadata="onMetadataLoaded"
+            @canplaythrough="onVideoReady">
         </video>
     </div>
 </template>
 
 <style lang="scss" module>
-    .container {
-        
-    }
+.container {}
 </style>
 
 <script>
@@ -47,19 +26,19 @@ export default {
             type: Function,
             required: true,
         },
-        
+
     },
-    created(){
+    created() {
         this.objectUrl = null;
     },
-    mounted(){
+    mounted() {
         // have to do this here, instead of immediate watcher, because otherwise the video element will not be mounted
         this.loadVideoFile();
     },
-    unmounted(){
+    unmounted() {
         URL.revokeObjectURL(this.objectUrl);
     },
-    data(){
+    data() {
         return {
             duration: 0,
             isVideoLoaded: false,
@@ -70,33 +49,36 @@ export default {
     computed: {
     },
     watch: {
-        videoFile(){
+        videoFile() {
             this.loadVideoFile();
         },
-        seekValue(newValue){
-            if(this.isVideoLoaded && newValue >= 0 && newValue <= this.duration){
+        seekValue(newValue) {
+            if (this.isVideoLoaded && newValue >= 0 && newValue <= this.duration) {
                 this.$refs.video.currentTime = newValue;
             }
         }
     },
     methods: {
-        loadVideoFile(){
+        loadVideoFile() {
             this.isVideoLoaded = false;
+            this.seekValue = -1;
             URL.revokeObjectURL(this.objectUrl);
             this.objectUrl = URL.createObjectURL(this.videoFile);
             const video = this.$refs.video;
             video.src = this.objectUrl;
             video.load();
         },
-        onSeeked(){
+        onSeeked() {
             this.onSeekChange(this.$refs.video);
         },
-        onMetadataLoaded(){
+        onMetadataLoaded() {
             this.duration = this.$refs.video.duration;
-            this.seekValue = 0;
         },
-        onVideoReady(){
-            this.isVideoLoaded = true;
+        onVideoReady() {
+            if (!this.isVideoLoaded) {
+                this.isVideoLoaded = true;
+                this.seekValue = 0;
+            }
         },
     }
 };
