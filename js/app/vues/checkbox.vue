da989848bb13deb8fa1b3f487aa85a43ed2e071d
Allen Garvey <allen.garvey@gmail.com>
2025-01-13 08:57:56 -0500

    Add margin between checkbox and label in checkbox component

--- js/app/vues/checkbox.vue
+++ js/app/vues/checkbox.vue
@@ -1,14 +1,21 @@
 <template>
-    <label :title="tooltip">{{ label }}
-        <input 
-            type="checkbox" 
-            :value="modelValue" 
-            @input="$emit('update:modelValue', !modelValue)" 
+    <label :title="tooltip"
+        ><span :class="$style.labelText">{{ label }}</span>
+        <input
+            type="checkbox"
+            :value="modelValue"
+            @input="$emit('update:modelValue', !modelValue)"
             :title="tooltip"
             :checked="modelValue"
         />
     </label>
 </template>
+
+<style lang="scss" module>
+.labelText {
+    margin-right: 0.25em;
+}
+</style>
 
 <script>
 export default {
