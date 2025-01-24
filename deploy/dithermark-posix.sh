91f75cb1f5bd5316a183e86898ebaca1abe416a7
Allen Garvey <allen.garvey@gmail.com>
2025-01-24 10:49:36 -0500

    Add posix shell script to start static webserver

---
+++ deploy/dithermark-posix.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PORT=8080
+HTML_ROOT="$(dirname "$0")/public_html"
+
+if [ -x "$(command -v python3)" ]; then
+    python3 -m http.server $PORT -d $HTML_ROOT
+elif [ -x "$(command -v php)" ]; then
+    php -S "localhost:$PORT" -t $HTML_ROOT
+else
+    >&2 echo "python3 or php is required to run $(basename "$0")"
+    return 1;
+fi
