d6ee34e644831cf90d3230eb9b0e08f849f91071
Allen Garvey <allen.garvey@gmail.com>
2025-01-27 11:14:35 -0500

    Update the go server to display the port it is listening on, and work when started from any directory

--- go-server/dithermark.go
+++ go-server/dithermark.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./public_html")))
-	http.ListenAndServe(":8080", nil)
+	executable, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	port := 8080
+	http.Handle("/", http.FileServer(http.Dir(path.Join(filepath.Dir(executable), "public_html"))))
+
+	fmt.Printf("Dithermark server listening on http://localhost:%d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
