29fb66e1ac6936975867ff7e502e356ee93a6f67
Allen Garvey <allen.garvey@gmail.com>
2025-01-30 10:26:47 -0500

    Add runtime config for go server to set port

--- go-server/dithermark.go
+++ go-server/dithermark.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,12 +9,41 @@ import (
 	"path/filepath"
 )
 
+type Config struct {
+	Port int `json:"port"`
+}
+
+func getPort(configPath string) int {
+	defaultPort := 8800
+
+	file, err := os.ReadFile(configPath)
+	if err != nil {
+		return defaultPort
+	}
+
+	var config Config
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err)
+		return defaultPort
+	}
+
+	if config.Port < 0 {
+		return defaultPort
+	}
+
+	return config.Port
+}
+
 func main() {
 	executable, err := os.Executable()
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
-	port := 8800
+	configPath := path.Join(filepath.Dir(executable), "config.json")
+
+	port := getPort(configPath)
 	http.Handle("/", http.FileServer(http.Dir(path.Join(filepath.Dir(executable), "public_html"))))
 
 	fmt.Printf("Dithermark server listening on http://localhost:%d\n", port)
