ed208a9f6cf04b9e893549e372daafdcef3a2a54
Allen Garvey <allen.garvey@gmail.com>
2025-01-30 10:50:34 -0500

    Add better error messages to go server when config is invalid

--- go-server/dithermark.go
+++ go-server/dithermark.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path"
@@ -24,11 +25,12 @@ func getPort(configPath string) int {
 	var config Config
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "Invalid config. Using default port: %d\n", defaultPort)
 		return defaultPort
 	}
 
 	if config.Port < 0 {
+		fmt.Fprintf(os.Stderr, "Port out of range. Using default port: %d\n", defaultPort)
 		return defaultPort
 	}
 
@@ -41,11 +43,24 @@ func main() {
 		fmt.Println(err)
 		panic(err)
 	}
-	configPath := path.Join(filepath.Dir(executable), "config.json")
+	configName := "config.json"
+	configPath := path.Join(filepath.Dir(executable), configName)
 
 	port := getPort(configPath)
 	http.Handle("/", http.FileServer(http.Dir(path.Join(filepath.Dir(executable), "public_html"))))
 
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to listen on port %d. Try changing the port in %s\n", port, configName)
+		os.Exit(1)
+	}
+
 	fmt.Printf("Dithermark server listening on http://localhost:%d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	serverErr := http.Serve(listener, nil)
+
+	if serverErr != nil {
+		fmt.Fprintf(os.Stderr, "Unable to start the server. Error: %s", serverErr)
+		os.Exit(1)
+	}
 }
