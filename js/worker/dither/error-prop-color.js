fa12e555a34c4c6be5c79e19ba28d5f69a0e11db
Allen Garvey <allen.garvey@gmail.com>
2025-10-09 13:02:04 -0400

    Fix exception in webworker color error prop dither due to reusing buffer for pixel transformation

--- js/worker/dither/error-prop-color.js
+++ js/worker/dither/error-prop-color.js
@@ -2,51 +2,65 @@ import Image from '../image.js';
 import ColorDitherModeFunctions from '../color-dither-mode-functions.js';
 import ErrorPropModel from './error-prop-model.js';
 
-
 /*
-** Error propagation matrix stuff
-*/
-function createErrorMaxtrix(width, numRows, lengthOffset, dimensions){
+ ** Error propagation matrix stuff
+ */
+function createErrorMaxtrix(width, numRows, lengthOffset, dimensions) {
     const rowWidth = (width + lengthOffset * 2) * dimensions;
     const data = {};
 
-    for(let i=0;i<numRows;i++){
+    for (let i = 0; i < numRows; i++) {
         data[i] = new Float32Array(rowWidth);
     }
-    
+
     return {
         dimensions,
         data,
     };
 }
 
-function errorMatrixIncrement(matrix, x, y, error, errorFraction){
+function errorMatrixIncrement(matrix, x, y, error, errorFraction) {
     const matrixValues = errorMatrixValue(matrix, x, y);
-    
-    for(let i=0;i<matrixValues.length;i++){
-        matrixValues[i] = matrixValues[i] + (error[i] * errorFraction);
+
+    for (let i = 0; i < matrixValues.length; i++) {
+        matrixValues[i] = matrixValues[i] + error[i] * errorFraction;
     }
 }
 
-function errorMatrixValue(matrix, x, y){
+function errorMatrixValue(matrix, x, y) {
     const normalizedX = x * matrix.dimensions;
-    return matrix.data[y].subarray(normalizedX, normalizedX + matrix.dimensions);
+    return matrix.data[y].subarray(
+        normalizedX,
+        normalizedX + matrix.dimensions
+    );
 }
 
 /**
  * Propagate error functions
-*/
-function propagateError(propagationModel, errorPropMatrix, x, currentError){
-    propagationModel.forEach((item)=>{
-        errorMatrixIncrement(errorPropMatrix, x + item[1], item[2], currentError, item[0]);
+ */
+function propagateError(propagationModel, errorPropMatrix, x, currentError) {
+    propagationModel.forEach(item => {
+        errorMatrixIncrement(
+            errorPropMatrix,
+            x + item[1],
+            item[2],
+            currentError,
+            item[0]
+        );
     });
 }
 
-
 /**
  * Base error prop functions
-*/
-function errorPropDitherBase(pixels, imageWidth, imageHeight, colorDitherModeId, colors, errorPropagationModel){
+ */
+function errorPropDitherBase(
+    pixels,
+    imageWidth,
+    imageHeight,
+    colorDitherModeId,
+    colors,
+    errorPropagationModel
+) {
     const colorDitherMode = ColorDitherModeFunctions[colorDitherModeId];
     const pixelValueFunc = colorDitherMode.pixelValue;
     const pixelDistanceFunc = colorDitherMode.distance;
@@ -54,46 +68,84 @@ function errorPropDitherBase(pixels, imageWidth, imageHeight, colorDitherModeId,
     const incrementValueFunc = colorDitherMode.incrementValue;
     const errorAmountFunc = colorDitherMode.errorAmount;
 
-    const colorValues = colors.map(pixelValueFunc);
-    const errorMatrix = createErrorMaxtrix(imageWidth, errorPropagationModel.numRows, errorPropagationModel.lengthOffset, modeDimensions);
+    const colorValues = colors.map(color => pixelValueFunc(color));
+    const errorMatrix = createErrorMaxtrix(
+        imageWidth,
+        errorPropagationModel.numRows,
+        errorPropagationModel.lengthOffset,
+        modeDimensions
+    );
     //this is to avoid uncessesary creation and deletion of arrays during error propagation
     const currentErrorBuffer = new Float32Array(modeDimensions);
     let errorMatrixIndex = errorPropagationModel.lengthOffset;
 
-    Image.transform(pixels, imageWidth, imageHeight, (pixel, x, y)=>{
-        const errorValue = errorMatrixValue(errorMatrix, errorMatrixIndex, 0);
-        const pixelAdjustedValue = incrementValueFunc(pixelValueFunc(pixel), errorValue);
-        const closestColorIndex = Image.findClosestColorIndex(pixelAdjustedValue, colorValues, pixelDistanceFunc);
-        
-        const closestColorValue = colorValues[closestColorIndex];
-        const currentError = errorAmountFunc(pixelAdjustedValue, closestColorValue, currentErrorBuffer);
-        propagateError(errorPropagationModel.matrix, errorMatrix, errorMatrixIndex, currentError);
+    Image.transform(
+        pixels,
+        imageWidth,
+        imageHeight,
+        (pixel, x, y) => {
+            const errorValue = errorMatrixValue(
+                errorMatrix,
+                errorMatrixIndex,
+                0
+            );
+            const pixelAdjustedValue = incrementValueFunc(
+                pixelValueFunc(pixel),
+                errorValue
+            );
+            const closestColorIndex = Image.findClosestColorIndex(
+                pixelAdjustedValue,
+                colorValues,
+                pixelDistanceFunc
+            );
 
-        errorMatrixIndex++;
-        pixel.set(colors[closestColorIndex]);
-        return pixel;
-    }, () => {
-        errorMatrixIndex = errorPropagationModel.lengthOffset;
-        // fill first row of error prop model with zero, 
-        //move it to the end and move all other rows up one
-        const temp = errorMatrix.data[0];
-        temp.fill(0);
-        const length = Object.keys(errorMatrix.data).length;
+            const closestColorValue = colorValues[closestColorIndex];
+            const currentError = errorAmountFunc(
+                pixelAdjustedValue,
+                closestColorValue,
+                currentErrorBuffer
+            );
+            propagateError(
+                errorPropagationModel.matrix,
+                errorMatrix,
+                errorMatrixIndex,
+                currentError
+            );
 
-        for(let i=1;i<length;i++){
-            errorMatrix.data[i-1] = errorMatrix.data[i];
+            errorMatrixIndex++;
+            pixel.set(colors[closestColorIndex]);
+            return pixel;
+        },
+        () => {
+            errorMatrixIndex = errorPropagationModel.lengthOffset;
+            // fill first row of error prop model with zero,
+            //move it to the end and move all other rows up one
+            const temp = errorMatrix.data[0];
+            temp.fill(0);
+            const length = Object.keys(errorMatrix.data).length;
+
+            for (let i = 1; i < length; i++) {
+                errorMatrix.data[i - 1] = errorMatrix.data[i];
+            }
+            errorMatrix.data[length - 1] = temp;
         }
-        errorMatrix.data[length-1] = temp;
-    });
+    );
 }
 
-function errorPropBuilder(propagateErrorModel){
-    return (pixels, imageWidth, imageHeight, colorDitherModeId, colors)=>{
-        return errorPropDitherBase(pixels, imageWidth, imageHeight, colorDitherModeId, colors, propagateErrorModel);
+function errorPropBuilder(propagateErrorModel) {
+    return (pixels, imageWidth, imageHeight, colorDitherModeId, colors) => {
+        return errorPropDitherBase(
+            pixels,
+            imageWidth,
+            imageHeight,
+            colorDitherModeId,
+            colors,
+            propagateErrorModel
+        );
     };
 }
 
-export default{
+export default {
     floydSteinberg: errorPropBuilder(ErrorPropModel.floydSteinberg()),
     javisJudiceNinke: errorPropBuilder(ErrorPropModel.javisJudiceNinke()),
     stucki: errorPropBuilder(ErrorPropModel.stucki()),
@@ -103,5 +155,4 @@ export default{
     sierra1: errorPropBuilder(ErrorPropModel.sierra1()),
     atkinson: errorPropBuilder(ErrorPropModel.atkinson()),
     reducedAtkinson: errorPropBuilder(ErrorPropModel.reducedAtkinson()),
-    
 };
