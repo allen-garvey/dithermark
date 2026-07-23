3f905794dd23d0e86ea6881e2b8ba7322f98258b
Allen Garvey <allen.garvey@gmail.com>
2026-07-23 15:41:57 -0400

    Remove unnecessary cast to float in R2 Sequence webgl declaration

--- server/templates/shaders/fragment/dithers/shared/r2-sequence-declaration.glsl
+++ server/templates/shaders/fragment/dithers/shared/r2-sequence-declaration.glsl
@@ -2,6 +2,6 @@
 // and https://web.archive.org/web/20260202183215/https://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/
 
 float r2_sequence(vec2 v){
-    float value = mod(0.7548776662 * v.x + 0.56984029f * v.y, 1.0);
+    float value = mod(0.7548776662 * v.x + 0.56984029 * v.y, 1.0);
     return value < 0.5 ? 2.0 * value : 2.0 - 2.0 * value;
 }
