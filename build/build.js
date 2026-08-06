eca47bea27774fe34c77b7706a1a417636e4b682
Allen Garvey <allen.garvey@gmail.com>
2026-08-06 14:20:34 -0400

    Remove PHP files for php unsplash api server function

--- build/build.js
+++ build/build.js
@@ -3,7 +3,6 @@ import fs from 'fs/promises';
 
 import { PUBLIC_HTML_DIR } from './webpack.config.js';
 import { renderHome } from '../server/views/home.js';
-import { renderUnsplashDownloadApi } from '../server/views/unsplash-download-api.js';
 
 const isProduction = process.env.IS_PRODUCTION === 'true';
 
@@ -13,19 +12,4 @@ const homePromise = renderHome({ isProduction }).then(indexContent =>
     fs.writeFile(homeOutputPath, indexContent)
 );
 
-const SERVERLESS_PATH = path.join(import.meta.dirname, '..', 'serverless');
-
-const unsplashDownloadApiPromise = fs
-    .mkdir(SERVERLESS_PATH, { recursive: true })
-    .then(() => renderUnsplashDownloadApi())
-    .then(unsplashDownloadApi => {
-        if (unsplashDownloadApi) {
-            return fs.writeFile(
-                path.join(SERVERLESS_PATH, 'unsplash-download.php'),
-                unsplashDownloadApi
-            );
-        }
-        return null;
-    });
-
-Promise.all([homePromise, unsplashDownloadApiPromise]);
+Promise.all([homePromise]);
